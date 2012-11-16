b6b295c745b1c989d3878fb6fadcfa01fd1a3c03
Alexandre Fayolle @ camptocamp <alexandre.fayolle@camptocamp.com>
2012-11-16 15:54:10 +0100

    [MRG] manual merge of recent changeset in bzr branch lp:~techreceptives/+junk/tr_barcode
    I requested a license clarification from Tech Receptive some time ago, and so now we have the official license statement from the original authors of the addon

--- tr_barcode/__openerp__.py
+++ tr_barcode/__openerp__.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+#/#############################################################################
+#    
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2004-TODAY Tech-Receptives(<http://www.tech-receptives.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+#/#############################################################################
 
 {
     'name': 'TR Barcode',
@@ -11,7 +30,7 @@ Presentation:
 This module adds the menu Barcode used to generate and configuration barcodes.
     
     """,
-    'author': 'Tech Receptives',
+    'author': 'Tech-Receptives Solutions Pvt. Ltd.',
     'website': 'http://www.techreceptives.com',
     'depends': [
         "base",
