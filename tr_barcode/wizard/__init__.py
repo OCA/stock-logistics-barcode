7cd057843f308d07c6d3be935b864823a85f0f76
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2012-11-19 12:36:28 +0100

    [DEL] Remove module cause we move them to lp:stock-logistic-tracking. Module List stock_barcode_reader

--- tr_barcode/wizard/__init__.py
+++ tr_barcode/wizard/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Tech-Receptives Solutions Pvt. Ltd.
-#    Copyright (C) 2004-TODAY Tech-Receptives(<http://www.tech-receptives.com>).
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
