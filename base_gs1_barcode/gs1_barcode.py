e6217cee87e0245349c8b0227a449d4c37f9fec1
Lionel Sausin <ls@numerigraphe.com>
2014-06-12 10:46:21 +0200

    [FIX] port base_gs1_barcode to v7
    Make base_gs1_barcode installable.
    Use a distinct exception. This exception is placed in the package's scope to make it easier to use from other modules. Now use "from base_gs1_barcode import InvalidGS1Barcode"
    Relicence as AGPL-3.
    Match coding conventions more closely.
    Fix typos.
    Add translation template.

--- base_gs1_barcode/gs1_barcode.py
+++ base_gs1_barcode/gs1_barcode.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    This module is copyright (C) 2012 Numérigraphe SARL. All Rights Reserved.
+#    This module is copyright (C) 2012-2014 Numérigraphe SARL.
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
@@ -24,44 +24,43 @@ from __future__ import division
 import re
 import time
 
-from openerp import netsvc
 from openerp.osv import orm, fields
 from openerp.tools.translate import _
+from .exceptions import InvalidGS1Barcode
 
-class invalid_gs1_barcode(orm.except_orm):
-    """Indicate an error occurred while decoding a GS1-128/GS1-Datamatrix code"""
-    pass
 
-class gs1_barcode(orm.Model):
+class GS1Barcode(orm.Model):
     """GS1-128/GS1-Datamatrix barcode decoder API and configuration"""
     _name = "gs1_barcode"
     _description = __doc__
     _columns = {
-        'ai' : fields.char('Application Identifer', size=14,
-                           help='The standard Application Identifier (AI)',
-                           required=1, select=1),
-        'name': fields.char('Description', size=64, required=True, select=1,
-                            translate=True),
-        'length_fixed': fields.boolean('Fixed-length Data',
-                                       help='Indicates whether the length of '
-                                            'the data for this Application '
-                                            'Identifier is fixed or not.'),
-        'length_max': fields.integer('Maximum Data Length',
-                                     help='Maximum length of the data for this '
-                                          'Application Identifier.', required=1),
-        'length_min': fields.integer('Minimum Data Length',
-                                     help='Minimum length of the data for '
-                                          'this Application Identifier.'),
-        'decimal': fields.boolean('Decimal Indicator',
-                                  help='Indicates whether a digit is expected '
-                                       'before the data for this Application '
-                                       'Identifier to indicate the position of '
-                                       'the decimal point.'
-                                  ),
-        'type': fields.selection([ ('string', 'Any character string'),
-                                    ('numeric', 'Numeric value'),
-                                    ('date', 'Date') ],
-                                    'Data Type', required=1),
+        'ai': fields.char(
+            'Application Identifer', size=14,
+            help='The standard Application Identifier (AI)', required=1,
+            select=1),
+        'name': fields.char(
+            'Description', size=64, required=True, select=1, translate=True),
+        'length_fixed': fields.boolean(
+            'Fixed-length Data',
+            help="Indicates whether the length of the data for this "
+                 "Application Identifier is fixed or not."),
+        'length_max': fields.integer(
+            'Maximum Data Length',
+            help="Maximum length of the data for this Application Identifier.",
+            required=1),
+        'length_min': fields.integer(
+            'Minimum Data Length',
+            help="Minimum length of the data for this Application "
+                 "Identifier."),
+        'decimal': fields.boolean(
+            'Decimal Indicator',
+            help="Indicates whether a digit is expected before the data for "
+                 "this Application Identifier to indicate the position of "
+                 "the decimal point."),
+        'type': fields.selection(
+            [('string', 'Any character string'),
+             ('numeric', 'Numeric value'),
+             ('date', 'Date')], 'Data Type', required=1),
     }
     _defaults = {
         'length_fixed': True,
@@ -70,7 +69,8 @@ class gs1_barcode(orm.Model):
         'type': 'string',
     }
     _sql_constraints = [
-        ('ai_uniq', 'unique (ai)', 'The Application Identifier must be unique!'),
+        ('ai_uniq', 'unique (ai)',
+         'The Application Identifier must be unique!'),
     ]
     _order = 'ai'
 
@@ -78,8 +78,8 @@ class gs1_barcode(orm.Model):
         """
         Decode a GS1-128/GS1-Datamatrix string to dictionary of values with
         Application Identifiers as keys.
-        Please note that the string MUST contain a <GS> character (group
-        separator) after each variable-length value. <GS> is usually
+        Please note that the string MUST contain a <GS> character (Group
+        Separator) after each variable-length value. <GS> is usually
         expected to be sent as ASCII character 29 but that may be configured
         per user.
 
@@ -88,19 +88,19 @@ class gs1_barcode(orm.Model):
 
         @type  barcode_string: string
         @param barcode_string: GS1-128/GS1-Datamatrix string  to decode
-        @return:               A dictionary of values with Application Identifiers as keys
+        @return: A dictionary of values with Application Identifiers as keys
         """
 
         # Prefix and Group Separator
         user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
         prefix = user.gs1_barcode_prefix or ''
-        separator = user.gs1_barcode_separator or '\x1D'
+        sep = user.gs1_barcode_separator or '\x1D'
 
         if not barcode_string.startswith(prefix):
-            raise invalid_gs1_barcode(_('Error decoding barcode'),
-                                 _('Could not decode barcode : '
-                                   'wrong prefix - the code should '
-                                   'start with "%s"') % prefix)
+            raise InvalidGS1Barcode(
+                _('Error decoding barcode'),
+                _('Could not decode barcode : wrong prefix - the code should '
+                  'start with "%s"') % prefix)
 
         # We are going to use lots of regular expressions to decode the string,
         # and they all boil down to the following templates:
@@ -114,12 +114,12 @@ class gs1_barcode(orm.Model):
         # * regular expression to match a variable length value ending with
         #   a <GS> character, to the group called "value".
         #   Must be formated with a pair of integers.
-        VARIABLE_LENGTH = r'(?P<value>[^' + separator + r']{%d,%d}' + separator + r'?)'
+        VARIABLE_LENGTH = r'(?P<value>[^' + sep + r']{%d,%d}' + sep + r'?)'
         #  * regular expression to match the position of the decimal separator
         #    after the AI code, to the group called "decimal".
         DECIMAL = r'(?P<decimal>\d)'
 
-        # Make a dictionary of compiled regular expressions to decode the string
+        # Make a dictionary of compiled regexps to decode the string
         ai_regexps = {}
         value_regexps = {}
         types = {}
@@ -154,20 +154,23 @@ class gs1_barcode(orm.Model):
 
                     # We found the Application Identifier, now decode the value
                     try:
-                        groups = value_regexps[ai].match(barcode_string, position).groupdict()
+                        groups = value_regexps[ai].match(
+                            barcode_string, position).groupdict()
                     except AttributeError:
-                        raise invalid_gs1_barcode(_('Error decoding barcode'),
-                                             _('Could not decode barcode: '
-                                               'incorrect value for Application '
-                                               'Identifer "%s" at position %d') % (ai, position))
+                        raise InvalidGS1Barcode(
+                            _('Error decoding barcode'),
+                            _('Could not decode barcode: incorrect value for '
+                              'Application Identifer "%s" at position %d') % (
+                                  ai, position))
 
                     position += len(groups['value'])
-                    results[ai] = groups['value'].replace(separator, '')
+                    results[ai] = groups['value'].replace(sep, '')
                     if types[ai] == 'numeric':
                         results[ai] = int(results[ai])
                         if 'decimal' in groups:
                             # Account for the decimal position
-                            results[ai] = results[ai] / (10 ** int(groups['decimal']))
+                            results[ai] = results[ai] / (
+                                10 ** int(groups['decimal']))
                             position += len(groups['decimal'])
                     if types[ai] == 'date':
                         # Format the date
@@ -182,10 +185,10 @@ class gs1_barcode(orm.Model):
                     # We know we won't match another AI for now, move on
                     break
             else:
-                # We couldn't find another valid AI in the rest of the code, give up
-                raise invalid_gs1_barcode(_('Error decoding barcode'),
-                                      _('Could not decode barcode: '
-                                        'unknown Application Identifier '
-                                        'at position %d') % position)
+                # No other valid AI found in the rest of the code: give up
+                raise InvalidGS1Barcode(
+                    _('Error decoding barcode'),
+                    _('Could not decode barcode: unknown Application '
+                      'Identifier at position %d') % position)
 
         return results
