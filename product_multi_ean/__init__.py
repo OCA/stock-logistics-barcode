85543e0dd69abb3c988e147d9612366b84ec6a55
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-15 02:34:30 +0100

    [IMP] product_multi_ean: Refactoring + code optimization
    * Tests
    * Spanish translation
    * Improve label
    * Text search + order

--- product_multi_ean/__init__.py
+++ product_multi_ean/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import models
+from .hooks import post_init_hook
