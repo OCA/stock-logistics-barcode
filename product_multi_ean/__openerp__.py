522edfe12eae87c6f3088e95ac943ef764e20110
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-15 02:34:30 +0100

    [IMP] product_multi_ean: Refactoring + code optimization
    * Tests
    * Spanish translation
    * Improve label
    * Text search + order

--- product_multi_ean/__openerp__.py
+++ product_multi_ean/__openerp__.py
@@ -1,34 +1,18 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author:  Author Guewen Baconnier
-#    Copyright 2012-2014 Camptocamp SA
-#    Author:  Roberto Lizana
-#    Copyright 2015 Trey, Kilobytes de Soluciones
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2012-2014 Guewen Baconnier (Camptocamp SA)
+# © 2015 Roberto Lizana (Trey)
+# © 2016 Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Multiple EAN13 on products',
-    'version': '1.3',
+    'version': '8.0.1.0.0',
     'license': 'AGPL-3',
-    'author': "Camptocamp,"
-              "Trey,"
+    'author': "Camptocamp, "
+              "Trey, "
+              "Tecnativa, "
               "Odoo Community Association (OCA)",
-    'category': 'Warehouse',
+    'category': 'Product Management',
     'depends': ['product'],
     'website': 'https://github.com/OCA/stock-logistics-barcode',
     'data': [
@@ -36,4 +20,5 @@
         'security/ir.model.access.csv',
     ],
     'installable': True,
+    'post_init_hook': 'post_init_hook',
 }
