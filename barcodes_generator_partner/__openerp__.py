c8fe8233a9ed6d548192847f3e55d9ec6784aece
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-11-08 20:43:22 +0100

    new module barcode_generator_partner

---
+++ barcodes_generator_partner/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014-Today GRAP (http://www.grap.coop)
+# Copyright (C) 2016-Today La Louve (http://www.lalouve.net)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Generate Barcodes for Partners',
+    'summary': 'Generate Barcodes for Partners',
+    'version': '9.0.1.0.0',
+    'category': 'Tools',
+    'author':
+        'GRAP,'
+        'La Louve,'
+        'Odoo Community Association (OCA)',
+    'website': 'http://www.odoo-community.org',
+    'license': 'AGPL-3',
+    'depends': [
+        'barcodes_generator_abstract',
+        'point_of_sale',
+    ],
+    'data': [
+        'views/view_res_partner.xml',
+    ],
+    'demo': [
+        'demo/ir_sequence.xml',
+        'demo/barcode_rule.xml',
+        'demo/res_partner.xml',
+        'demo/function.xml',
+    ],
+}
