914c8f680b6b8bee416a295ed1aa4e34d6f76ede
Dave Lasley <dave@laslabs.com>
2016-12-28 13:37:26 -0800

    [MIG] barcodes_generator_abstract: Upgrade to v10 * Rename manifest * Change openerp references to odoo * Bump version * Alphabetize imports * pyBarcode is no longer maintained, switch to viivakoodi fork (https://bitbucket.org/whitie/python-barcode/issues/16/pypi-08-release-request#comment-33978213)

--- barcodes_generator_abstract/models/barcode_rule.py
+++ barcodes_generator_abstract/models/barcode_rule.py
@@ -4,7 +4,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, _, exceptions
+from odoo import _, api, exceptions, fields, models
 
 _GENERATE_TYPE = [
     ('no', 'No generation'),
