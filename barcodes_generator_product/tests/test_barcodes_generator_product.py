14fcd637491f23abde49ea7527c550b58dd37b16
Dave Lasley <dave@laslabs.com>
2016-12-28 13:52:41 -0800

    [MIG] barcodes_product_generator: Upgrade to v10 * Rename manifest * Change openerp references to odoo * Bump version * Fix spelling errors and add product avatar to icon * Remove product_barcode_generator

--- barcodes_generator_product/tests/test_barcodes_generator_product.py
+++ barcodes_generator_product/tests/test_barcodes_generator_product.py
@@ -4,7 +4,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class Tests(TransactionCase):
