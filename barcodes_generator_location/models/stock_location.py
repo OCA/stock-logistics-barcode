201623ecc28780f1dc11b5841f5bcc0adcb27137
Dave Lasley <dave@laslabs.com>
2017-02-21 17:01:59 -0800

    Fix link, verbiage, and lint

--- barcodes_generator_location/models/stock_location.py
+++ barcodes_generator_location/models/stock_location.py
@@ -2,7 +2,7 @@
 # Copyright 2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import models
 
 
 class StockLocation(models.Model):
