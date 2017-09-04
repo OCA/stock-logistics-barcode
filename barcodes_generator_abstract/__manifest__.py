bc47264412718bc58d2863bfc2010d7460026f4a
Dave Lasley <dave@laslabs.com>
2017-09-04 09:27:30 -0700

    [10.0][FIX] barcodes_generator_abstract: Caching issue (#103)
    * [FIX] barcodes_generator_abstract: Caching issue
    * Make cache method return ids & move the recordset method out of cache to fix #93

--- barcodes_generator_abstract/__manifest__.py
+++ barcodes_generator_abstract/__manifest__.py
@@ -7,7 +7,7 @@
 {
     'name': 'Generate Barcodes (Abstract)',
     'summary': 'Generate Barcodes for Any Models',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'Tools',
     'author':
         'GRAP, '
