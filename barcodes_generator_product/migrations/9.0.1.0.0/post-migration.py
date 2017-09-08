ed6fffeed7cc7ec407c71d5a69be5c9273f33ad2
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-09-08 15:24:04 +0200

    [IMP] barcodes_generator_product: OpenUpgrade migration script
    Script for migrations coming from v8 module product_barcode_generator

---
+++ barcodes_generator_product/migrations/9.0.1.0.0/post-migration.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+
+def convert_barcode_sequences(env):
+    """This converts the sequences found with a barcode to barcode rules for
+    being selectable at product level."""
+    if not openupgrade.column_exists(
+            env.cr, 'ir_sequence', 'barcode_sequence'
+    ):
+        # Return if product_barcode_generator was not previously installed
+        return
+    rule_obj = env['barcode.rule']
+    env.cr.execute("""
+        SELECT id, name, barcode_sequence
+        FROM ir_sequence
+        WHERE barcode_sequence IS NOT NULL
+        """)
+    for row in env.cr.fetchall():
+        rule_obj.create({
+            'name': row[1],
+            'type': 'product',
+            'generate_model': 'product.product',
+            'encoding': 'ean13',
+            'pattern': row[2] + "." * (12 - len(row[2])),
+            'generate_type': 'sequence',
+            'sequence_id': row[0],
+            'barcode_nomenclature_id': env.ref(
+                'barcodes.default_barcode_nomenclature'
+            ).id,
+        })
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    convert_barcode_sequences(env)
