4e77050a793c34bb9c8e2f74f765fe3b283011df
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-10-24 00:55:58 +0200

    wip

--- mobile_app_inventory/models/stock_inventory.py
+++ mobile_app_inventory/models/stock_inventory.py
@@ -38,11 +38,9 @@ class StockInventory(models.Model):
         return res
 
     @api.multi
-    def add_inventory_line_by_scan(
-            self, location_id, product_id, qty, mode):
+    def mobile_add_inventory_line(self, location_id, product_id, qty, mode):
         """
         Add a new line in the current inventory
-        @param id: the inventory id;
         @param location_id, product_id ;
         @qty : the quantity to set or to add depending of the mode;
         @mode :
@@ -57,69 +55,66 @@ class StockInventory(models.Model):
             {'state': 'duplicate', qty: xxx}:
                 There is a duplicate. Qty is the current quantity
         """
-
-        silo = self.env['stock.inventory.line']
-        inventory_id = self.id
-        product = self.env['product.product'].browse(product_id)
-        ilines = silo.search([
-            ('inventory_id', '=', inventory_id),
-            ('product_id', '=', product.id),
-            ])
-        if len(ilines) == 1:
-            ilines[0].product_qty = qty
-        elif len(ilines) > 1:
-            return {'warning': {
-                'title': _('Error'),
-                'message': _(
-                    'Several inventory lines exists for this product. '
-                    'This scenario is not supported for the moment. '
-                    'It may be caused by the fact that you have '
-                    'sub-locations in the stock location of this '
-                    'inventory and this product is present in '
-                    'several sub-locations, or by the fact that you '
-                    'track Serial Numbers for this product.')}}
-        else:
-            new_iline = silo.create({
-                'inventory_id': inventory_id,
-                'location_id': self.location_id.id,
-                'product_id': product.id,
-                'product_uom_id': product.uom_id.id,
-                'product_qty': qty,
-                })
-            self.inventory_line_id = new_iline
-        return {'state': 'write_ok'}
-
-        # TODO FIXME
-        # EUH ??? C'est en cours de refactoring ce machin.
-
+        self.ensure_one()
         qty = float(qty)
         product_obj = self.env['product.product']
         line_obj = self.env['stock.inventory.line']
         product = product_obj.browse(product_id)
 
         # Check if there is existing line with the product
-        line_ids = line_obj.search([
+        lines = line_obj.search([
             ('inventory_id', '=', self.id),
             ('location_id', '=', location_id),
             ('product_id', '=', product_id)])
-        if not line_ids:
+        if not lines:
             line_vals = {
                 'location_id': location_id,
                 'product_id': product_id,
-                'product_uom': product.uom_id.id,
+                'product_uom_id': product.uom_id.id,
                 'product_qty': qty,
             }
-            inventory_vals = {'inventory_line_id': [[0, False, line_vals]]}
+            inventory_vals = {'line_ids': [[0, False, line_vals]]}
             self.write(inventory_vals)
             return {'state': 'write_ok'}
-        elif len(line_ids) == 1:
-            line = line_obj.browse(line_ids[0])
+        elif len(lines) == 1:
             if mode == 'ask':
-                return {'state': 'duplicate', 'qty': line.product_qty}
+                return {'state': 'duplicate', 'qty': lines[0].product_qty}
             elif mode == 'add':
-                qty += line.product_qty
-            line_obj.write(
-                 line_ids[0], {'product_qty': qty})
+                qty += lines[0].product_qty
+            lines[0].write({'product_qty': qty})
             return {'state': 'write_ok'}
         else:
             return {'state': 'many_duplicate_lines'}
+
+        # Current Raphael Code to check.
+        # Pas compris
+#        silo = self.env['stock.inventory.line']
+#        inventory_id = self.id
+#        product = self.env['product.product'].browse(product_id)
+#        ilines = silo.search([
+#            ('inventory_id', '=', inventory_id),
+#            ('product_id', '=', product.id),
+#            ])
+#        if len(ilines) == 1:
+#            ilines[0].product_qty = qty
+#        elif len(ilines) > 1:
+#            return {'warning': {
+#                'title': _('Error'),
+#                'message': _(
+#                    'Several inventory lines exists for this product. '
+#                    'This scenario is not supported for the moment. '
+#                    'It may be caused by the fact that you have '
+#                    'sub-locations in the stock location of this '
+#                    'inventory and this product is present in '
+#                    'several sub-locations, or by the fact that you '
+#                    'track Serial Numbers for this product.')}}
+#        else:
+#            new_iline = silo.create({
+#                'inventory_id': inventory_id,
+#                'location_id': self.location_id.id,
+#                'product_id': product.id,
+#                'product_uom_id': product.uom_id.id,
+#                'product_qty': qty,
+#                })
+#            self.inventory_line_id = new_iline
+#        return {'state': 'write_ok'}
--- mobile_app_inventory/static/www/controllers/c_select_quantity.js
+++ mobile_app_inventory/static/www/controllers/c_select_quantity.js
@@ -14,27 +14,26 @@ angular.module('mobile_app_inventory').controller(
     $scope.$on(
             '$stateChangeSuccess',
             function(event, toState, toParams, fromState, fromParams){
-        $scope.data.fixed_qty = false;
-        // TODO remove location loading (useless now. (just for back button))
-        StockLocationModel.get_location(toParams.location_id).then(function (location) {
-            $scope.data.location = location;
-        })
-        // TODO remove inventory loading (useless now).
-        StockInventoryModel.get_inventory(toParams.inventory_id).then(function(inventory) {
-            $scope.data.inventory = inventory;
-        });
-        ProductProductModel.get_product(toParams.ean13).then(function(product) {
-            if (product['barcode_qty'] !== undefined) {
-                console.log("barcode qty");
-                $scope.data.fixed_qty = true;
-                $scope.data.qty = parseFloat(product['barcode_qty']);
-            }
-            console.log(">>>>>>>>>>>>>>>>>>>>>");
-            console.log(product);
-            $scope.data.product = product;
-        });
-        angular.element(document.querySelector('#input_quantity'))[0].focus();
-        $scope.data.qty = '';
+        if ($state.current.name === 'product_ean13') {
+            $scope.data.fixed_qty = false;
+            // TODO remove location loading (useless now. (just for back button))
+            StockLocationModel.get_location(toParams.location_id).then(function (location) {
+                $scope.data.location = location;
+            })
+            // TODO remove inventory loading (useless now).
+            StockInventoryModel.get_inventory(toParams.inventory_id).then(function(inventory) {
+                $scope.data.inventory = inventory;
+            });
+            ProductProductModel.get_product(toParams.ean13).then(function(product) {
+                if (product['barcode_qty'] !== undefined) {
+                    $scope.data.fixed_qty = true;
+                    $scope.data.qty = parseFloat(product['barcode_qty']);
+                }
+                $scope.data.product = product;
+            });
+            angular.element(document.querySelector('#input_quantity'))[0].focus();
+            $scope.data.qty = '';
+        }
     });
 
     $scope.submit = function () {
@@ -51,11 +50,9 @@ angular.module('mobile_app_inventory').controller(
             return;
         }
 
-        StockInventoryModel.AddInventoryLine(
-                $scope.data.inventory.id,
-                $scope.data.location.id,
-                $scope.data.product.id,
-                parsed_qty, 'ask').then(function (res){
+        StockInventoryModel.add_inventory_line(
+                $scope.data.inventory.id, $scope.data.location.id,
+                $scope.data.product.id, parsed_qty, 'ask').then(function (res){
             if (res.state == 'write_ok'){
                 angular.element(document.querySelector('#sound_quantity_selected'))[0].play();
                 $state.go('product', {
@@ -65,6 +62,8 @@ angular.module('mobile_app_inventory').controller(
             }else {
                 if (res.state == 'duplicate'){
                     $state.go('confirm_quantity', {
+                        inventory_id: $scope.data.inventory.id,
+                        location_id: $scope.data.location.id,
                         product_id: $scope.data.product.id,
                         current_qty: res.qty,
                         new_qty: parsed_qty});
