9676f3d713dbeaceefacc772abd31442b2a975b2
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-10-27 17:22:53 +0200

    New API

--- mobile_app_inventory/models/__init__.py
+++ mobile_app_inventory/models/__init__.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+
+# To remove at the end of the refactoring
 from . import res_users
 from . import stock_inventory
 from . import stock_location
 from . import product_product
 from . import res_company
+
+# New API
+from . import mobile_app_inventory
