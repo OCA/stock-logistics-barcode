344f2f9eadb874667f9743f34d5e8c5c97b10023
phatnguyenuit <phatnt.uit@gmail.com>
2018-01-12 18:08:48 +0700

    [MIG] barcodes_generator_abstract: Migration to 11.0

--- barcodes_generator_abstract/models/__init__.py
+++ barcodes_generator_abstract/models/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
-from . import barcode_generate_mixin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import barcode_generate_mixin
 from . import barcode_rule
