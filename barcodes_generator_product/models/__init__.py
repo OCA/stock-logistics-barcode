7719c7b97c9453df4bf26b9fed13fe5d8e4e8a60
phatnguyenuit <phatnt.uit@gmail.com>
2018-01-15 11:55:34 +0700

    [MIG] barcodes_generator_product: Migration to 11.0

--- barcodes_generator_product/models/__init__.py
+++ barcodes_generator_product/models/__init__.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import barcode_rule
 from . import product_product
 from . import product_template
