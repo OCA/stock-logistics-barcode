c6d22682c6a181187989a7d62e1dcaeea0be63db
phatnguyenuit <phatnt.uit@gmail.com>
2018-01-15 11:55:34 +0700

    [MIG] barcodes_generator_product: Migration to 11.0

--- barcodes_generator_product/__manifest__.py
+++ barcodes_generator_product/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2014-Today GRAP (http://www.grap.coop)
 # Copyright (C) 2016-Today La Louve (http://www.lalouve.net)
+# Copyright (C) 2018 Komit (https://komit-consulting.com)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Generate Barcodes for Products',
     'summary': 'Generate Barcodes for Products (Templates and Variants)',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Tools',
     'author':
         'GRAP,'
