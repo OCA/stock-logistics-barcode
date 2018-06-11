0884d5f20507172f51fc4a092cee56846aad79ed
xavierjimenez <xavier.jimenez@qubiq.es>
2018-06-11 16:45:02 +0200

    [MIG] product_multi_ean: Migration to 11.0

--- product_multi_ean/__manifest__.py
+++ product_multi_ean/__manifest__.py
@@ -1,19 +1,22 @@
-# -*- coding: utf-8 -*-
 # © 2012-2014 Guewen Baconnier (Camptocamp SA)
 # © 2015 Roberto Lizana (Trey)
 # © 2016 Pedro M. Baeza
+# © 2018 Xavier Jimenez (QubiQ)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Multiple EAN13 on products',
-    'version': '8.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': "Camptocamp, "
               "Trey, "
               "Tecnativa, "
+              "QubiQ, "
               "Odoo Community Association (OCA)",
     'category': 'Product Management',
-    'depends': ['product'],
+    'depends': [
+        'product',
+    ],
     'website': 'https://github.com/OCA/stock-logistics-barcode',
     'data': [
         'views/product_view.xml',
--- product_multi_ean/hooks.py
+++ product_multi_ean/hooks.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 # © 2015 Roberto Lizana (Trey)
 # © 2016 Pedro M. Baeza
+# © 2018 Xavier Jimenez (QubiQ)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
@@ -8,6 +8,6 @@ def post_init_hook(cr, registry):
     cr.execute("""
     INSERT INTO product_ean13
     (product_id, name, sequence)
-    SELECT id, ean13, 0
+    SELECT id, barcode, 0
     FROM product_product
-    WHERE ean13 IS NOT NULL""")
+    WHERE barcode IS NOT NULL""")
