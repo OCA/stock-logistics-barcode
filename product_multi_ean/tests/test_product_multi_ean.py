abaa14667223cbf3e0a3fa0648f8528009f747bc
xavierjimenez <xavier.jimenez@qubiq.es>
2018-06-11 16:45:02 +0200

    [MIG] product_multi_ean: Migration to 11.0

--- product_multi_ean/tests/test_product_multi_ean.py
+++ product_multi_ean/tests/test_product_multi_ean.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
 # © 2016 Pedro M. Baeza
+# © 2018 Xavier Jimenez (QubiQ)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests import common
+from odoo.tests.common import TransactionCase
 from ..hooks import post_init_hook
 
 
-class TestProductMultiEan(common.TransactionCase):
+class TestProductMultiEan(TransactionCase):
     def setUp(self):
         super(TestProductMultiEan, self).setUp()
         self.product = self.env['product.product'].create({
@@ -16,16 +16,16 @@ class TestProductMultiEan(common.TransactionCase):
         self.valid_ean2 = '0123456789012'
 
     def test_set_main_ean(self):
-        self.product.ean13 = self.valid_ean
+        self.product.barcode = self.valid_ean
         self.assertEqual(len(self.product.ean13_ids), 1)
-        self.assertEqual(self.product.ean13_ids.name, self.product.ean13)
+        self.assertEqual(self.product.ean13_ids.name, self.product.barcode)
 
     def test_set_incorrect_ean(self):
         with self.assertRaises(Exception):
-            self.product.ean13 = '1234567890123'
+            self.product.barcode = '1234567890123'
         with self.assertRaises(Exception):
             self.product.ean13_ids = [(0, 0, {'name': '1234567890123'})]
-        self.product.ean13 = self.valid_ean
+        self.product.barcode = self.valid_ean
         # Insert duplicated EAN13
         with self.assertRaises(Exception):
             self.product.ean13_ids = [(0, 0, {'name': self.valid_ean})]
@@ -33,7 +33,7 @@ class TestProductMultiEan(common.TransactionCase):
     def test_post_init_hook(self):
         self.env.cr.execute("""
             UPDATE product_product
-            SET ean13 = %s
+            SET barcode = %s
             WHERE id = %s""", (self.valid_ean, self.product.id))
         post_init_hook(self.env.cr, self.registry)
         self.product.refresh()
@@ -44,9 +44,9 @@ class TestProductMultiEan(common.TransactionCase):
         self.product.ean13_ids = [
             (0, 0, {'name': self.valid_ean}),
             (0, 0, {'name': self.valid_ean2})]
-        products = self.product.search([('ean13', '=', self.valid_ean)])
+        products = self.product.search([('barcode', '=', self.valid_ean)])
         self.assertEqual(len(products), 1)
         self.assertEqual(products, self.product)
-        products = self.product.search([('ean13', '=', self.valid_ean2)])
+        products = self.product.search([('barcode', '=', self.valid_ean2)])
         self.assertEqual(len(products), 1)
         self.assertEqual(products, self.product)
