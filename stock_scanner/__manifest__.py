295abcb9d6462f6be148249d70697d2ca7d54e63
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-06-28 10:27:03 +0200

    [FIX] stock_scanner: Bump module version number

--- stock_scanner/__manifest__.py
+++ stock_scanner/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Stock Scanner',
     'summary': 'Allows managing barcode readers with simple scenarios',
-    'version': '10.0.1.0.4',
+    'version': '10.0.1.1.0',
     'category': 'Generic Modules/Inventory Control',
     'website': 'https://odoo-community.org/',
     'author': 'SYLEAM,'
