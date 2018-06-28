daac05b1759ea00fcd0b3962ad982ed62c8357a4
Marçal Isern <misern2@users.noreply.github.com>
2018-06-28 10:14:50 +0200

    [FIX] stock_scanner: Added parent menu Stock for stock_scanner_XXX modules (#149)

--- stock_scanner/models/scanner_hardware.py
+++ stock_scanner/models/scanner_hardware.py
@@ -735,14 +735,18 @@ class ScannerHardware(models.Model):
         """
         Retrieve the scenario list for this warehouse
         """
-
         scanner_scenario_obj = self.env['scanner.scenario']
         scanner_scenario_ids = scanner_scenario_obj.search([
             '|',
             ('warehouse_ids', '=', False),
             ('warehouse_ids', 'in', [self.warehouse_id.id]),
+
             ('parent_id', '=', parent_id),
+            '|',
+            ('child_ids', '!=', False),
+            ('step_ids', '!=', False)
         ])
+
         return scanner_scenario_ids.mapped('name')
 
     @api.multi
