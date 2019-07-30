965b1bb4cc130c9306ced23e399230321fd2684d
Yurdik Cervantes Mendoza <ycervantes@elegosoft.com>
2019-07-30 10:22:32 +0200

    Update dependency in setup.py from viivakoodi to python-barcode
    The `python-barcode` project is active again and it seems to enhace
    the support for python3. It also uses `Pillow` instead of `PIL`.
    
    Ref: #93

--- setup/barcodes_generator_abstract/setup.py
+++ setup/barcodes_generator_abstract/setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     odoo_addon={
         'external_dependencies_override': {
             'python': {
-                'barcode': 'viivakoodi',
+                'barcode': 'python-barcode',
             },
         },
     }
