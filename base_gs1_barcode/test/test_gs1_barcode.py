469566e941087fdc193b509c1eb3b1deb41eef27
Media Engagers <38145455+mediaengagers@users.noreply.github.com>
2019-07-31 20:07:41 +0300

    [MIG] base_gs1_barcode: Migration to 12.0

--- base_gs1_barcode/test/test_gs1_barcode.py
+++ base_gs1_barcode/test/test_gs1_barcode.py
@@ -28,6 +28,21 @@ class GS1Barcode(common.TransactionCase):
         assert result.get('17') == expected_res_17
         assert result.get('10') == lot, "The lot should be %s" % lot
         assert result.get('310') == 0.06385, "The weight should be %s" % weight
+        # AI 311 (expiry date) - day 0 will be replaced with day 31
+        expiry = '140515'
+        # AI 311 (lot number, variable length)
+        lot = 'B04059A'
+        # AI 311 (Net Dimension in Meters, 5 decimals)
+        weight = '006385'
+        barcode = PREFIX + '01' + gtin + '311'
+        barcode += expiry + '10' + lot + GS + '3105' + weight
+        result = self.decode(barcode, context={})
+        assert len(result) == 4, "The barcode should decode to 4 AIs"
+        assert result.get('01') == gtin, "The GTIN should be %s" % gtin
+        expected_res_17 = '2014-05-31', "The expiry date should be 2014-05-31"
+        assert result.get('17') == expected_res_17
+        assert result.get('10') == lot, "The lot should be %s" % lot
+        assert result.get('310') == 0.06385, "The weight should be %s" % weight
         gtin = '03400933816759'
         # AI 17 (expiry date) - day 0 will be replaced with day 31
         expiry = '140522'
@@ -38,11 +53,6 @@ class GS1Barcode(common.TransactionCase):
         barcode = PREFIX + '01' + gtin + '17'
         barcode += expiry + '10' + lot + GS + '3105' + weight
         result = self.decode(barcode)
-        date_test = self.decode(barcode, context={}).normalize_date(expiry)
-        expiry_zero_end = '140500'
-        assert date_test == "2015-12-31"
-        date_test = self.decode(barcode).normalize_date(expiry_zero_end)
-        assert date_test == "2015-12-31"
         assert len(result) == 4, "The barcode should decode to 4 AIs"
         assert result.get('01') == gtin, "The GTIN should be %s" % gtin
         expected_res_17 = '2014-05-22', "The expiry date should be 2014-05-22"
