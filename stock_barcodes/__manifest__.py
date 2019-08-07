c2f6f00ac763320abc5bedb0f6af6500843dfe7a
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2019-08-07 09:02:29 +0200

    [ADD] stock_barcodes: New module to read barcodes on stocks operations (#195)

---
+++ stock_barcodes/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2019 Sergio Teruel <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Barcodes",
+    "summary": "It provides read barcode on stock operations.",
+    "version": "11.0.1.0.0",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "website": "https://odoo-community.org/",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "depends": [
+        'barcodes',
+        'stock',
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/assets.xml',
+        'views/stock_inventory_views.xml',
+        'wizard/stock_production_lot_views.xml',
+        'wizard/stock_barcodes_read_views.xml',
+        'wizard/stock_barcodes_read_inventory_views.xml',
+    ],
+    'installable': True,
+}
