8a4e4d795239144fc07255e1b68e91e08f10773d
Andrea <a.stirpe@onestein.nl>
2019-09-03 10:13:01 +0200

    [12.0][MIG] stock_barcodes

--- stock_barcodes/static/src/js/stock_barcodes.js
+++ stock_barcodes/static/src/js/stock_barcodes.js
@@ -1,7 +1,7 @@
 /* Copyright 2018-2019 Sergio Teruel <sergio.teruel@tecnativa.com>.
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
-odoo.define('stock_barcodes.FormController', function(require) {
+odoo.define('stock_barcodes.FormController', function (require) {
     'use strict';
 
     var FormController = require('web.FormController');
@@ -13,19 +13,19 @@ odoo.define('stock_barcodes.FormController', function(require) {
             Set control focus to package_qty or product_qty directly after
             scan a barcode for manual entry mode entries.
             */
-            this._super(barcode, target).then(function(){
+            this._super(barcode, target).then(function () {
                 var manual_entry_mode = self.$("div[name='manual_entry'] input").val();
-                if (manual_entry_mode){
+                if (manual_entry_mode) {
                     var packaging = self.$("div[name='packaging_id'] input").val();
                     if (packaging) {
                         self.$("input[name='packaging_qty']").focus();
                     } else {
                         self.$("input[name='product_qty']").focus();
-                    };
-                };
+                    }
+                }
             });
         },
-        renderButtons: function($node){
+        renderButtons: function ($node) {
             /*
             Hide save and discard buttons from wizard, for this form do
             anything and confuse the user if he wants do a manual entry. All
@@ -33,10 +33,19 @@ odoo.define('stock_barcodes.FormController', function(require) {
             buttons.
             */
             this._super($node);
-            if (this.modelName.includes('wiz.stock.barcodes.read.')){
+            if (this.modelName.includes('wiz.stock.barcodes.read.')) {
                 this.$buttons.find('.o_form_buttons_edit')
                     .css({'display': 'none'});
             }
+        },
+        canBeDiscarded: function (recordID) {
+            /*
+             Silent the warning that says that the record has been modified.
+             */
+            if (!this.modelName.includes('wiz.stock.barcodes.read.')) {
+                return this._super.apply(this, arguments);
+            }
+            return $.when(false);
         },
     });
 });
