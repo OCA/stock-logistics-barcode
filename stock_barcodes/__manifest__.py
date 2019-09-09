519551317e15129c1483a9c3e52cdb89f103b2c2
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-09-09 13:24:59 +0200

    [ADD] stock_barcodes: Allow to read barcodes from pickings

--- stock_barcodes/__manifest__.py
+++ stock_barcodes/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Barcodes",
     "summary": "It provides read barcode on stock operations.",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "website": "https://odoo-community.org/",
@@ -17,9 +17,11 @@
         'security/ir.model.access.csv',
         'views/assets.xml',
         'views/stock_inventory_views.xml',
+        'views/stock_picking_views.xml',
         'wizard/stock_production_lot_views.xml',
         'wizard/stock_barcodes_read_views.xml',
         'wizard/stock_barcodes_read_inventory_views.xml',
+        'wizard/stock_barcodes_read_picking_views.xml',
     ],
     'installable': True,
 }
--- stock_barcodes/readme/DESCRIPTION.rst
+++ stock_barcodes/readme/DESCRIPTION.rst
@@ -4,4 +4,4 @@ This module contains a base wizard read barcode that can be extended by
 other modules.
 
 This module also makes use of this wizard for providing barcode support for
-doing inventories.
+doing inventories and picking operations.
