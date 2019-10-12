6cdb0a9ee390d90dd3ce7d90e52419ef8a4c8c34
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-10-12 16:52:43 +0200

    [IMP] stock_barcodes: Hidden lot for non tracking products. When user creates a lot, set default product the wizard selected product. When confirm the lot creation wizard, set this lot in lot wizard scanning barcode field.

--- stock_barcodes/tests/test_stock_barcodes_new_lot.py
+++ stock_barcodes/tests/test_stock_barcodes_new_lot.py
@@ -1,10 +1,10 @@
 # Copyright 2108-2019 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.addons.stock_barcodes.tests.test_stock_barcodes import\
-    TestStockBarcodes
+from odoo.addons.stock_barcodes.tests.test_stock_barcodes_inventory import\
+    TestStockBarcodesInventory
 
 
-class TestStockBarcodesNewLot(TestStockBarcodes):
+class TestStockBarcodesNewLot(TestStockBarcodesInventory):
 
     def setUp(self):
         super().setUp()
@@ -16,5 +16,9 @@ class TestStockBarcodesNewLot(TestStockBarcodes):
         self.assertEqual(self.wiz_scan_lot.product_id, self.product_tracking)
         self.action_barcode_scanned(self.wiz_scan_lot, '8433281xy6850')
         self.assertEqual(self.wiz_scan_lot.lot_name, '8433281xy6850')
-        new_lot = self.wiz_scan_lot.confirm()
+        new_lot = self.wiz_scan_lot.with_context(
+            active_model=self.wiz_scan_inventory._name,
+            active_id=self.wiz_scan_inventory.id,
+        ).confirm()
         self.assertEqual(self.wiz_scan_lot.lot_name, new_lot.name)
+        self.assertEqual(self.wiz_scan_inventory.lot_id, new_lot)
