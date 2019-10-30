256c32a267da71326a1af4d1ebcdcfe784661dc8
Florent de Labarre <florent.mirieu@gmail.com>
2019-10-30 22:16:46 +0100

    [FIX] stock_scanner : copy doesn't work

--- stock_scanner/tests/test_stock_scanner_scenario.py
+++ stock_scanner/tests/test_stock_scanner_scenario.py
@@ -14,3 +14,16 @@ class TestStockScannerScenario(common.TransactionCase):
             'stock_scanner.scanner_scenario_sentinel')
         with self.assertRaises(exceptions.ValidationError):
             parent_scenario.parent_id = child_scenario
+
+    def test_copy_scenario(self):
+        scenario = self.env.ref(
+            'stock_scanner.scanner_scenario_tutorial')
+        copied_scenario = scenario.copy()
+
+        self.assertEqual(len(scenario.step_ids), len(copied_scenario.step_ids))
+
+        transitions = scenario.step_ids.mapped('out_transition_ids') \
+            | scenario.step_ids.mapped('in_transition_ids')
+        copied_transitions = copied_scenario.step_ids.mapped('out_transition_ids') \
+            | copied_scenario.step_ids.mapped('in_transition_ids')
+        self.assertEqual(len(transitions), len(copied_transitions))
