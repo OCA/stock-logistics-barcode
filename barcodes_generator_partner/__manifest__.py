d12dd06df4d414eefc1872081df1768ebc663ff3
Georg A. G. Notter <gn@agenterp.com>
2019-12-03 20:54:05 +0100

    fix(barcodes_generator_partner): remove dependency to point of sale and add dep to stock
    place the fields in misc

--- barcodes_generator_partner/__manifest__.py
+++ barcodes_generator_partner/__manifest__.py
@@ -4,27 +4,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Generate Barcodes for Partners',
-    'summary': 'Generate Barcodes for Partners',
-    'version': '11.0.1.0.0',
-    'category': 'Tools',
-    'author':
-        'GRAP,'
-        'La Louve,'
-        'Odoo Community Association (OCA)',
-    'website': 'https://www.odoo-community.org',
-    'license': 'AGPL-3',
-    'depends': [
-        'barcodes_generator_abstract',
-        'point_of_sale',
-    ],
-    'data': [
-        'views/view_res_partner.xml',
-    ],
-    'demo': [
-        'demo/ir_sequence.xml',
-        'demo/barcode_rule.xml',
-        'demo/res_partner.xml',
-        'demo/function.xml',
+    "name": "Generate Barcodes for Partners",
+    "summary": "Generate Barcodes for Partners",
+    "version": "11.0.1.0.0",
+    "category": "Tools",
+    "author": "GRAP," "La Louve," "Odoo Community Association (OCA)",
+    "website": "https://www.odoo-community.org",
+    "license": "AGPL-3",
+    "depends": ["stock", "barcodes_generator_abstract"],
+    "data": ["views/view_res_partner.xml"],
+    "demo": [
+        "demo/ir_sequence.xml",
+        "demo/barcode_rule.xml",
+        "demo/res_partner.xml",
+        "demo/function.xml",
     ],
 }
