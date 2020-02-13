1ff913da94024750f2cb085c8b07985ff1dfda89
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-13 19:22:34 +0100

    [IMP] stock_barcodes: black, isort

--- stock_barcodes/models/stock_inventory.py
+++ stock_barcodes/models/stock_inventory.py
@@ -4,21 +4,21 @@ from odoo import models
 
 
 class StockInventory(models.Model):
-    _inherit = ['barcodes.barcode_events_mixin', 'stock.inventory']
-    _name = 'stock.inventory'
+    _inherit = ["barcodes.barcode_events_mixin", "stock.inventory"]
+    _name = "stock.inventory"
 
     def action_barcode_scan(self):
         action = self.env.ref(
-            'stock_barcodes.action_stock_barcodes_read_inventory').read()[0]
-        action['context'] = {
-            'default_location_id': self.location_id.id,
-            'default_product_id': self.product_id.id,
-            'default_prod_lot_id': self.lot_id.id,
-            'default_package_id': self.package_id.id,
-            'default_partner_id': self.partner_id.id,
-            'default_inventory_id': self.id,
-            'default_res_model_id':
-                self.env.ref('stock.model_stock_inventory').id,
-            'default_res_id': self.id,
+            "stock_barcodes.action_stock_barcodes_read_inventory"
+        ).read()[0]
+        action["context"] = {
+            "default_location_id": self.location_id.id,
+            "default_product_id": self.product_id.id,
+            "default_prod_lot_id": self.lot_id.id,
+            "default_package_id": self.package_id.id,
+            "default_partner_id": self.partner_id.id,
+            "default_inventory_id": self.id,
+            "default_res_model_id": self.env.ref("stock.model_stock_inventory").id,
+            "default_res_id": self.id,
         }
         return action
