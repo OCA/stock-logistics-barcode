2561d2b876a544c575408eff50691b836f532efb
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-13 19:22:35 +0100

    [MIG] stock_barcodes: Migration to v13.0

--- stock_barcodes/__manifest__.py
+++ stock_barcodes/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Barcodes",
     "summary": "It provides read barcode on stock operations.",
-    "version": "12.0.1.1.1",
+    "version": "13.0.1.0.0",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-barcode",
     "license": "AGPL-3",
