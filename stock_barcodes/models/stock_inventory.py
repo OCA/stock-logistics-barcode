c397a029147acb432bcf59114249970a123355aa
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-13 19:22:35 +0100

    [MIG] stock_barcodes: Migration to v13.0

--- stock_barcodes/models/stock_inventory.py
+++ stock_barcodes/models/stock_inventory.py
@@ -8,15 +8,15 @@ class StockInventory(models.Model):
     _name = "stock.inventory"
 
     def action_barcode_scan(self):
+        self.start_empty = True
+        self._action_start()
+        self._check_company()
         action = self.env.ref(
             "stock_barcodes.action_stock_barcodes_read_inventory"
         ).read()[0]
         action["context"] = {
-            "default_location_id": self.location_id.id,
-            "default_product_id": self.product_id.id,
-            "default_prod_lot_id": self.lot_id.id,
-            "default_package_id": self.package_id.id,
-            "default_partner_id": self.partner_id.id,
+            "default_location_id": self.location_ids[:1].id,
+            "default_product_id": self.product_ids[:1].id,
             "default_inventory_id": self.id,
             "default_res_model_id": self.env.ref("stock.model_stock_inventory").id,
             "default_res_id": self.id,
