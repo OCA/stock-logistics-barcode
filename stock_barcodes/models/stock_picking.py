7500b6cfcd23fdcb5ff81f672b52445a0abf2663
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-13 19:22:34 +0100

    [IMP] stock_barcodes: black, isort

--- stock_barcodes/models/stock_picking.py
+++ stock_barcodes/models/stock_picking.py
@@ -4,18 +4,18 @@ from odoo import models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     def action_barcode_scan(self):
         action = self.env.ref(
-            'stock_barcodes.action_stock_barcodes_read_picking').read()[0]
-        action['context'] = {
-            'default_location_id': self.location_dest_id.id,
-            'default_partner_id': self.partner_id.id,
-            'default_picking_id': self.id,
-            'default_res_model_id':
-                self.env.ref('stock.model_stock_picking').id,
-            'default_res_id': self.id,
-            'default_picking_type_code': self.picking_type_code,
+            "stock_barcodes.action_stock_barcodes_read_picking"
+        ).read()[0]
+        action["context"] = {
+            "default_location_id": self.location_dest_id.id,
+            "default_partner_id": self.partner_id.id,
+            "default_picking_id": self.id,
+            "default_res_model_id": self.env.ref("stock.model_stock_picking").id,
+            "default_res_id": self.id,
+            "default_picking_type_code": self.picking_type_code,
         }
         return action
--- stock_barcodes/tests/test_stock_barcodes_picking.py
+++ stock_barcodes/tests/test_stock_barcodes_picking.py
@@ -1,141 +1,174 @@
 # Copyright 2108-2019 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.addons.stock_barcodes.tests.test_stock_barcodes import\
-    TestStockBarcodes
+from odoo.addons.stock_barcodes.tests.test_stock_barcodes import TestStockBarcodes
 
 
 class TestStockBarcodesPicking(TestStockBarcodes):
     def setUp(self):
         super().setUp()
-        self.ScanReadPicking = self.env['wiz.stock.barcodes.read.picking']
-        self.stock_picking_model = self.env.ref('stock.model_stock_picking')
+        self.ScanReadPicking = self.env["wiz.stock.barcodes.read.picking"]
+        self.stock_picking_model = self.env.ref("stock.model_stock_picking")
 
         # Model Data
-        self.partner_agrolite = self.env.ref('base.res_partner_2')
-        self.picking_type_in = self.env.ref('stock.picking_type_in')
-        self.picking_type_out = self.env.ref('stock.picking_type_out')
-        self.supplier_location = self.env.ref('stock.stock_location_suppliers')
-        self.customer_location = self.env.ref('stock.stock_location_customers')
-        self.stock_location = self.env.ref('stock.stock_location_stock')
-        self.categ_unit = self.env.ref('uom.product_uom_categ_unit')
-        self.categ_kgm = self.env.ref('uom.product_uom_categ_kgm')
-        self.picking_out_01 = self.env['stock.picking'].with_context(
-            planned_picking=True
-        ).create({
-            'location_id': self.stock_location.id,
-            'location_dest_id': self.customer_location.id,
-            'partner_id': self.partner_agrolite.id,
-            'picking_type_id': self.picking_type_out.id,
-            'move_lines': [
-                (0, 0, {
-                    'name': self.product_tracking.name,
-                    'product_id': self.product_tracking.id,
-                    'product_uom_qty': 3,
-                    'product_uom': self.product_tracking.uom_id.id,
-                    'location_id': self.stock_location.id,
-                    'location_dest_id': self.customer_location.id,
-                }),
-            ]
-        })
+        self.partner_agrolite = self.env.ref("base.res_partner_2")
+        self.picking_type_in = self.env.ref("stock.picking_type_in")
+        self.picking_type_out = self.env.ref("stock.picking_type_out")
+        self.supplier_location = self.env.ref("stock.stock_location_suppliers")
+        self.customer_location = self.env.ref("stock.stock_location_customers")
+        self.stock_location = self.env.ref("stock.stock_location_stock")
+        self.categ_unit = self.env.ref("uom.product_uom_categ_unit")
+        self.categ_kgm = self.env.ref("uom.product_uom_categ_kgm")
+        self.picking_out_01 = (
+            self.env["stock.picking"]
+            .with_context(planned_picking=True)
+            .create(
+                {
+                    "location_id": self.stock_location.id,
+                    "location_dest_id": self.customer_location.id,
+                    "partner_id": self.partner_agrolite.id,
+                    "picking_type_id": self.picking_type_out.id,
+                    "move_lines": [
+                        (
+                            0,
+                            0,
+                            {
+                                "name": self.product_tracking.name,
+                                "product_id": self.product_tracking.id,
+                                "product_uom_qty": 3,
+                                "product_uom": self.product_tracking.uom_id.id,
+                                "location_id": self.stock_location.id,
+                                "location_dest_id": self.customer_location.id,
+                            },
+                        )
+                    ],
+                }
+            )
+        )
         self.picking_out_02 = self.picking_out_01.copy()
-        self.picking_in_01 = self.env['stock.picking'].with_context(
-            planned_picking=True
-        ).create({
-            'location_id': self.supplier_location.id,
-            'location_dest_id': self.stock_location.id,
-            'partner_id': self.partner_agrolite.id,
-            'picking_type_id': self.picking_type_in.id,
-            'move_lines': [
-                (0, 0, {
-                    'name': self.product_wo_tracking.name,
-                    'product_id': self.product_wo_tracking.id,
-                    'product_uom_qty': 3,
-                    'product_uom': self.product_wo_tracking.uom_id.id,
-                    'location_id': self.supplier_location.id,
-                    'location_dest_id': self.stock_location.id,
-                }),
-                (0, 0, {
-                    'name': self.product_wo_tracking.name,
-                    'product_id': self.product_wo_tracking.id,
-                    'product_uom_qty': 5,
-                    'product_uom': self.product_wo_tracking.uom_id.id,
-                    'location_id': self.supplier_location.id,
-                    'location_dest_id': self.stock_location.id,
-                }),
-                (0, 0, {
-                    'name': self.product_tracking.name,
-                    'product_id': self.product_tracking.id,
-                    'product_uom_qty': 3,
-                    'product_uom': self.product_tracking.uom_id.id,
-                    'location_id': self.supplier_location.id,
-                    'location_dest_id': self.stock_location.id,
-                }),
-                (0, 0, {
-                    'name': self.product_tracking.name,
-                    'product_id': self.product_tracking.id,
-                    'product_uom_qty': 5,
-                    'product_uom': self.product_tracking.uom_id.id,
-                    'location_id': self.supplier_location.id,
-                    'location_dest_id': self.stock_location.id,
-                }),
-            ]
-        })
+        self.picking_in_01 = (
+            self.env["stock.picking"]
+            .with_context(planned_picking=True)
+            .create(
+                {
+                    "location_id": self.supplier_location.id,
+                    "location_dest_id": self.stock_location.id,
+                    "partner_id": self.partner_agrolite.id,
+                    "picking_type_id": self.picking_type_in.id,
+                    "move_lines": [
+                        (
+                            0,
+                            0,
+                            {
+                                "name": self.product_wo_tracking.name,
+                                "product_id": self.product_wo_tracking.id,
+                                "product_uom_qty": 3,
+                                "product_uom": self.product_wo_tracking.uom_id.id,
+                                "location_id": self.supplier_location.id,
+                                "location_dest_id": self.stock_location.id,
+                            },
+                        ),
+                        (
+                            0,
+                            0,
+                            {
+                                "name": self.product_wo_tracking.name,
+                                "product_id": self.product_wo_tracking.id,
+                                "product_uom_qty": 5,
+                                "product_uom": self.product_wo_tracking.uom_id.id,
+                                "location_id": self.supplier_location.id,
+                                "location_dest_id": self.stock_location.id,
+                            },
+                        ),
+                        (
+                            0,
+                            0,
+                            {
+                                "name": self.product_tracking.name,
+                                "product_id": self.product_tracking.id,
+                                "product_uom_qty": 3,
+                                "product_uom": self.product_tracking.uom_id.id,
+                                "location_id": self.supplier_location.id,
+                                "location_dest_id": self.stock_location.id,
+                            },
+                        ),
+                        (
+                            0,
+                            0,
+                            {
+                                "name": self.product_tracking.name,
+                                "product_id": self.product_tracking.id,
+                                "product_uom_qty": 5,
+                                "product_uom": self.product_tracking.uom_id.id,
+                                "location_id": self.supplier_location.id,
+                                "location_dest_id": self.stock_location.id,
+                            },
+                        ),
+                    ],
+                }
+            )
+        )
         self.picking_in_01.action_confirm()
         vals = self.picking_in_01.action_barcode_scan()
         self.wiz_scan_picking = self.ScanReadPicking.with_context(
-            vals['context']
+            vals["context"]
         ).create({})
 
     def test_wiz_picking_values(self):
-        self.assertEqual(self.wiz_scan_picking.location_id,
-                         self.picking_in_01.location_dest_id)
-        self.assertEqual(self.wiz_scan_picking.res_model_id,
-                         self.stock_picking_model)
-        self.assertEqual(self.wiz_scan_picking.res_id,
-                         self.picking_in_01.id)
-        self.assertEqual(self.wiz_scan_picking.display_name,
-                         'Barcode reader - %s - OdooBot' % (
-                             self.picking_in_01.name))
+        self.assertEqual(
+            self.wiz_scan_picking.location_id, self.picking_in_01.location_dest_id
+        )
+        self.assertEqual(self.wiz_scan_picking.res_model_id, self.stock_picking_model)
+        self.assertEqual(self.wiz_scan_picking.res_id, self.picking_in_01.id)
+        self.assertEqual(
+            self.wiz_scan_picking.display_name,
+            "Barcode reader - %s - OdooBot" % (self.picking_in_01.name),
+        )
 
     def test_picking_wizard_scan_product(self):
-        self.action_barcode_scanned(self.wiz_scan_picking, '8480000723208')
-        self.assertEqual(
-            self.wiz_scan_picking.product_id, self.product_wo_tracking)
+        self.action_barcode_scanned(self.wiz_scan_picking, "8480000723208")
+        self.assertEqual(self.wiz_scan_picking.product_id, self.product_wo_tracking)
         sml = self.picking_in_01.move_line_ids.filtered(
-            lambda x: x.product_id == self.product_wo_tracking)
+            lambda x: x.product_id == self.product_wo_tracking
+        )
         self.assertEqual(sml.qty_done, 1.0)
         # Scan product with tracking lot enable
-        self.action_barcode_scanned(self.wiz_scan_picking, '8433281006850')
+        self.action_barcode_scanned(self.wiz_scan_picking, "8433281006850")
         sml = self.picking_in_01.move_line_ids.filtered(
-            lambda x: x.product_id == self.product_tracking)
+            lambda x: x.product_id == self.product_tracking
+        )
         self.assertEqual(sml.qty_done, 0.0)
-        self.assertEqual(self.wiz_scan_picking.message,
-                         'Barcode: 8433281006850 (Waiting for input lot)')
+        self.assertEqual(
+            self.wiz_scan_picking.message,
+            "Barcode: 8433281006850 (Waiting for input lot)",
+        )
         # Scan a lot. Increment quantities if scan product or other lot from
         # this product
-        self.action_barcode_scanned(self.wiz_scan_picking, '8411822222568')
+        self.action_barcode_scanned(self.wiz_scan_picking, "8411822222568")
         sml = self.picking_in_01.move_line_ids.filtered(
-            lambda x: x.product_id == self.product_tracking and x.lot_id)
+            lambda x: x.product_id == self.product_tracking and x.lot_id
+        )
         self.assertEqual(sml.lot_id, self.lot_1)
         self.assertEqual(sml.qty_done, 1.0)
-        self.action_barcode_scanned(self.wiz_scan_picking, '8433281006850')
+        self.action_barcode_scanned(self.wiz_scan_picking, "8433281006850")
         self.assertEqual(sml.qty_done, 2.0)
-        self.action_barcode_scanned(self.wiz_scan_picking, '8411822222568')
+        self.action_barcode_scanned(self.wiz_scan_picking, "8411822222568")
         self.assertEqual(sml.qty_done, 3.0)
-        self.assertEqual(self.wiz_scan_picking.message,
-                         'Barcode: 8411822222568 (Barcode read correctly)')
+        self.assertEqual(
+            self.wiz_scan_picking.message,
+            "Barcode: 8411822222568 (Barcode read correctly)",
+        )
         # Scan a package
-        self.action_barcode_scanned(self.wiz_scan_picking, '5420008510489')
+        self.action_barcode_scanned(self.wiz_scan_picking, "5420008510489")
         # Package of 5 product units. Already three unit exists
         self.assertEqual(sml.qty_done, 8.0)
 
     def test_picking_wizard_scan_product_manual_entry(self):
         self.wiz_scan_picking.manual_entry = True
-        self.action_barcode_scanned(self.wiz_scan_picking, '8480000723208')
-        self.assertEqual(self.wiz_scan_picking.product_id,
-                         self.product_wo_tracking)
+        self.action_barcode_scanned(self.wiz_scan_picking, "8480000723208")
+        self.assertEqual(self.wiz_scan_picking.product_id, self.product_wo_tracking)
         sml = self.picking_in_01.move_line_ids.filtered(
-            lambda x: x.product_id == self.product_wo_tracking)
+            lambda x: x.product_id == self.product_wo_tracking
+        )
         self.assertEqual(self.wiz_scan_picking.product_qty, 0.0)
         self.wiz_scan_picking.product_qty = 12.0
         self.wiz_scan_picking.action_manual_entry()
@@ -143,11 +176,11 @@ class TestStockBarcodesPicking(TestStockBarcodes):
         self.assertEqual(sml.move_id.quantity_done, 12.0)
 
     def test_picking_wizard_remove_last_scan(self):
-        self.action_barcode_scanned(self.wiz_scan_picking, '8480000723208')
-        self.assertEqual(self.wiz_scan_picking.product_id,
-                         self.product_wo_tracking)
+        self.action_barcode_scanned(self.wiz_scan_picking, "8480000723208")
+        self.assertEqual(self.wiz_scan_picking.product_id, self.product_wo_tracking)
         sml = self.picking_in_01.move_line_ids.filtered(
-            lambda x: x.product_id == self.product_wo_tracking)
+            lambda x: x.product_id == self.product_wo_tracking
+        )
         self.assertEqual(sml.qty_done, 1.0)
         self.wiz_scan_picking.action_undo_last_scan()
         self.assertEqual(sml.qty_done, 0.0)
@@ -160,7 +193,7 @@ class TestStockBarcodesPicking(TestStockBarcodes):
 
         vals = self.picking_type_out.action_barcode_scan()
         self.wiz_scan_picking = self.ScanReadPicking.with_context(
-            vals['context']
+            vals["context"]
         ).create({})
         self.wiz_scan_picking.manual_entry = True
         self.wiz_scan_picking.product_id = self.product_tracking
@@ -171,10 +204,10 @@ class TestStockBarcodesPicking(TestStockBarcodes):
         self.assertEqual(len(self.wiz_scan_picking.candidate_picking_ids), 2)
         # Lock first picking
         candidate = self.wiz_scan_picking.candidate_picking_ids.filtered(
-            lambda c: c.picking_id == self.picking_out_01)
+            lambda c: c.picking_id == self.picking_out_01
+        )
         candidate_wiz = candidate.with_context(
-            wiz_barcode_id=self.wiz_scan_picking.id,
-            picking_id=self.picking_out_01.id,
+            wiz_barcode_id=self.wiz_scan_picking.id, picking_id=self.picking_out_01.id
         )
         candidate_wiz.action_lock_picking()
         self.assertEqual(self.picking_out_01.move_lines.quantity_done, 2)
