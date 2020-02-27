0fd95bc23b93897e5379d35b4de8f3f38f1307eb
ahenriquez <ahenriquez@eficent.com>
2020-02-27 12:45:39 +0100

    upt

--- stock_barcodes_json/wizard/stock_barcodes_read.py
+++ stock_barcodes_json/wizard/stock_barcodes_read.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import _, fields, models
+from odoo import _, api, fields, models
 from odoo.tools.safe_eval import safe_eval
 
 
@@ -78,14 +78,51 @@ class WizStockBarcodesMappingItem(models.Model):
     _description = 'Barcode Item'
     _rec_name = 'key'
 
+    @api.model
+    def _get_valid_fields(self):
+        return [('product_id', 'Product'),
+                ('product_qty', 'Product Quantity'),
+                ('product_barcode', 'Product Barcode'),
+                ('lot_barcode', 'Lot Barcode'),
+                ('package_barcode', 'Package Barcode')]
+
     mapping_id = fields.Many2one('wiz.stock.barcodes.mapping')
-    key = fields.Char() # to do: selection to ir_fields
-    value = fields.Char()
+    key = fields.Char(required=True) # to do: selection to ir_fields
+    value = fields.Selection(_get_valid_fields, required=True)
 
 class WizStockBarcodesMapping(models.Model):
     _name = 'wiz.stock.barcodes.mapping'
     _description = 'Barcode Mapping'
     _rec_name = 'model_id'
 
-    model_id = fields.Many2one('ir.model')
-    item_ids = fields.One2many('wiz.stock.barcodes.mapping.item', 'mapping_id')
+    model_id = fields.Many2one('ir.model', required=True)
+    model_label_id = fields.Many2one('ir.model', string="Model of the label" , required=True)
+    item_ids = fields.One2many('wiz.stock.barcodes.mapping.item', 'mapping_id', required=True)
+
+
+    @api.model
+    def prepare_label(self, model, items):
+        json = "{"
+        for item in items:
+            json += "'%s': '%s'," % (item.key, item.value)
+        json += "}"
+        return {'name': 'Label for '+ model.name,
+                'sequence': 32,
+                'model_id': model.id,
+                'component_ids': [(0,0,{'name': 'QR json',
+                'component_type': 'qr_code',
+                'magnification_factor': 5,
+                'data': json,
+                })]
+        }
+
+    @api.multi
+    def create_zpl2_label(self):
+        self.ensure_one()
+        # here we will create zpl2 label with a json
+        label_vals = self.prepare_label(self.model_label_id, self.item_ids)
+        label = self.env['printing.label.zpl2'].create(label_vals)
+        action = self.env.ref('printer_zpl2.act_open_printing_label_zpl2_view')
+        action = action.read()[0]
+        action['res_id'] = label.id
+        return action
