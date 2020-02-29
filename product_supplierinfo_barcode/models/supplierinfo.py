677318ae87c678f9ab872ffc8b2151fddf4877ba
eLBati <lb@takobi.online>
2020-02-29 07:44:06 +0100

    ADD product_supplierinfo_barcode: Add barcode field to supplier pricelis items (supplier info), so that it can used to search product by supplier barcodes.

---
+++ product_supplierinfo_barcode/models/supplierinfo.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+
+class SupplierInfo(models.Model):
+    _inherit = "product.supplierinfo"
+
+    barcode = fields.Char(
+        'Barcode', copy=False,
+        help="Article number used by supplier for product identification.")
+
+    _sql_constraints = [(
+        'barcode_uniq', 'unique(barcode)',
+        "A barcode can only be assigned to one product !")]
