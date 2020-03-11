2f35aae5f09cb85f64c20be83cc5525a508bdd32
jim.hoefnagels <jim.hoefnagels@dynapps.be>
2020-03-11 08:58:42 +0100

    [IMP] product_multi_ean: black, isort

--- product_multi_ean/__manifest__.py
+++ product_multi_ean/__manifest__.py
@@ -5,23 +5,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Multiple EAN13 on products',
-    'version': '12.0.1.0.1',
-    'license': 'AGPL-3',
-    'author': "Camptocamp, "
-              "Trey, "
-              "Tecnativa, "
-              "QubiQ, "
-              "Odoo Community Association (OCA)",
-    'category': 'Product Management',
-    'depends': [
-        'product',
-    ],
-    'website': 'https://github.com/OCA/stock-logistics-barcode',
-    'data': [
-        'views/product_view.xml',
-        'security/ir.model.access.csv',
-    ],
-    'installable': True,
-    'post_init_hook': 'post_init_hook',
+    "name": "Multiple EAN13 on products",
+    "version": "12.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "Camptocamp, "
+    "Trey, "
+    "Tecnativa, "
+    "QubiQ, "
+    "Odoo Community Association (OCA)",
+    "category": "Product Management",
+    "depends": ["product"],
+    "website": "https://github.com/OCA/stock-logistics-barcode",
+    "data": ["views/product_view.xml", "security/ir.model.access.csv"],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
 }
