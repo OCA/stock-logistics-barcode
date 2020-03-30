b11f4bc01f33cf03414115abb6f2f42e6de62727
hveficent <hector.villarreal@forgeflow.com>
2020-03-30 18:23:59 +0200

    [MIG]stock_barcodes_move_location v13

--- stock_barcodes_move_location/wizard/stock_barcodes_read_move_location.py
+++ stock_barcodes_move_location/wizard/stock_barcodes_read_move_location.py
@@ -1,9 +1,23 @@
 # Copyright 2019 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import logging
+import subprocess
+from subprocess import PIPE
+
 from odoo import _, fields, models
 from odoo.fields import first
+from odoo.tools.safe_eval import safe_eval
 
-from odoo.addons import decimal_precision as dp
+_logger = logging.getLogger(__name__)
+
+
+def find_between(s, first, last):
+    try:
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
 
 
 class WizStockBarcodesReadMoveLocation(models.TransientModel):
@@ -12,16 +26,41 @@ class WizStockBarcodesReadMoveLocation(models.TransientModel):
     _description = "Wizard to read barcode on move location"
 
     move_location_id = fields.Many2one(
-        comodel_name="wiz.stock.move.location", string="Move Location", readonly=True,
+        comodel_name="wiz.stock.move.location", string="Move Location", readonly=True
     )
     move_location_qty = fields.Float(
-        string="To Move quantities",
-        digits=dp.get_precision("Product Unit of Measure"),
-        readonly=True,
+        string="To Move quantities", digits="Product Unit of Measure", readonly=True,
     )
     location_dest_id = fields.Many2one(
-        comodel_name="stock.location", string="Destination Location", readonly=True,
+        comodel_name="stock.location", string="Destination Location", readonly=True
     )
+
+    def action_zbarcam(self):
+        res = False
+        proc = subprocess.Popen(["zbarcam"], stdout=PIPE, stderr=PIPE)
+        (res, error) = proc.communicate()
+        _logger.info(res)
+        barcodes = str(res.decode("utf-8"))
+        # TODO move to stock_barcodes_zbarcam module
+        # TODO implement other barcodes types
+        qr = find_between(barcodes, "QR-Code:", "\n")
+        if qr:
+            qr = qr.replace("}", "},")  # for multiple lectures
+            qr = "[" + qr + "]"
+            qr = safe_eval(qr)
+            for bar in qr:
+                self.barcode = str(bar)
+                self.reset_qty()
+                self.process_barcode(str(bar))
+                self.action_manual_entry()
+        code128 = find_between(barcodes, "CODE-128:", "\n")
+        if code128:
+            code128 = code128.split(",")
+            for bar in code128:
+                self.barcode = str(bar)
+                self.reset_qty()
+                self.process_barcode(str(bar))
+                self.action_manual_entry()
 
     def name_get(self):
         return [
@@ -44,7 +83,7 @@ class WizStockBarcodesReadMoveLocation(models.TransientModel):
         # Apply the putaway strategy
         move_location_dest_id = self.move_location_id.destination_location_id
         self.location_dest_id = (
-            self.move_location_id.destination_location_id.get_putaway_strategy(
+            self.move_location_id.destination_location_id._get_putaway_strategy(
                 self.product_id
             )
             or move_location_dest_id
@@ -65,9 +104,7 @@ class WizStockBarcodesReadMoveLocation(models.TransientModel):
             lambda x: (x.product_id == self.product_id and x.lot_id == self.lot_id)
         )
         if line:
-            line.write(
-                {"move_quantity": line.move_quantity + self.product_qty,}
-            )
+            line.write({"move_quantity": line.move_quantity + self.product_qty})
         else:
             line = MoveLocationLine.create(self._prepare_move_location_line())
             self.move_location_id.stock_move_location_line_ids = [(4, line.id)]
@@ -78,6 +115,7 @@ class WizStockBarcodesReadMoveLocation(models.TransientModel):
         if self.product_id.tracking != "none" and not self.lot_id:
             self._set_messagge_info("info", _("Waiting for input lot"))
             return False
+
         force_add_log = self.env.context.get("force_add_log", False)
         if self.manual_entry and not force_add_log:
             return False
@@ -106,7 +144,8 @@ class WizStockBarcodesReadMoveLocation(models.TransientModel):
             self.scan_log_ids.filtered(lambda x: x.create_uid == self.env.user)
         )
         if log_scan:
-            move_location_line = self.move_location_id.stock_move_location_line_ids.filtered(
+            sml_line_ids = self.move_location_id.stock_move_location_line_ids
+            move_location_line = sml_line_ids.filtered(
                 lambda x: (
                     x.product_id == log_scan.product_id and x.lot_id == log_scan.lot_id
                 )
@@ -117,7 +156,3 @@ class WizStockBarcodesReadMoveLocation(models.TransientModel):
                 self.move_location_qty = move_location_line.move_quantity
         log_scan.unlink()
         return res
-
-    def action_automatic_entry(self):
-        self._set_messagge_info("success", _("Barcode read correctly"))
-        self.action_manual_entry()
