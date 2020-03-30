8ed27ce18567d1c11a7bd4644e6425357c9e701a
ahenriquez <ahenriquez@eficent.com>
2020-03-30 18:23:20 +0200

    [IMP] : black, isort

--- stock_barcodes_move_location/__manifest__.py
+++ stock_barcodes_move_location/__manifest__.py
@@ -4,20 +4,19 @@
     "name": "Stock Barcodes",
     "summary": "It provides read barcode on stock operations.",
     "version": "12.0.2.0.0",
-    "author": "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://odoo-community.org/",
     "license": "AGPL-3",
     "category": "Extra Tools",
     "depends": [
-        'stock_barcodes',
-        'stock_move_location',
-        'stock_barcodes_automatic_entry',
+        "stock_barcodes",
+        "stock_move_location",
+        "stock_barcodes_automatic_entry",
     ],
     "data": [
-        'views/assets.xml',
-        'wizard/stock_move_location_views.xml',
-        'wizard/stock_barcodes_read_move_location_views.xml',
+        "views/assets.xml",
+        "wizard/stock_move_location_views.xml",
+        "wizard/stock_barcodes_read_move_location_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
