2860682a7d1f7c3e84b98a143fd175fbc9d80cc0
jim.hoefnagels <jim.hoefnagels@dynapps.be>
2020-04-09 12:11:03 +0200

    [IMP] stock_barcodes_gs1_expiry: black, isort

--- stock_barcodes_gs1_expiry/wizard/stock_barcodes_read.py
+++ stock_barcodes_gs1_expiry/wizard/stock_barcodes_read.py
@@ -4,21 +4,17 @@ from odoo import fields, models
 
 
 class WizStockBarcodesRead(models.AbstractModel):
-    _inherit = 'wiz.stock.barcodes.read'
+    _inherit = "wiz.stock.barcodes.read"
 
-    life_date = fields.Datetime(
-        string='End of Life Date',
-    )
-    use_date = fields.Datetime(
-        string='Best before Date',
-    )
+    life_date = fields.Datetime(string="End of Life Date",)
+    use_date = fields.Datetime(string="Best before Date",)
 
     def _prepare_lot_values(self, barcode_decoded):
         res = super()._prepare_lot_values(barcode_decoded)
-        preferred_date = barcode_decoded.get('15', False)
-        life_date = barcode_decoded.get('17', False)
+        preferred_date = barcode_decoded.get("15", False)
+        life_date = barcode_decoded.get("17", False)
         if preferred_date:
-            res['use_date'] = preferred_date
+            res["use_date"] = preferred_date
         if life_date:
-            res['life_date'] = life_date
+            res["life_date"] = life_date
         return res
