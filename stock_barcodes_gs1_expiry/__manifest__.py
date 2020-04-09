3dcbc616baf546eb900253e7c2d4a830b1da1624
jim.hoefnagels <jim.hoefnagels@dynapps.be>
2020-04-09 12:11:03 +0200

    [IMP] stock_barcodes_gs1_expiry: black, isort

--- stock_barcodes_gs1_expiry/__manifest__.py
+++ stock_barcodes_gs1_expiry/__manifest__.py
@@ -2,21 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Barcodes GS1 Expiry",
-    "summary": "It provides read expiry dates from GS1 barcode on "
-               "stock operations.",
+    "summary": "It provides read expiry dates from GS1 barcode on " "stock operations.",
     "version": "12.0.1.0.0",
-    "author": "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://odoo-community.org/",
     "license": "AGPL-3",
     "category": "Extra Tools",
-    "depends": [
-        'stock_barcodes_gs1',
-        'product_expiry',
-    ],
-    "data": [
-        'wizard/stock_production_lot_views.xml',
-    ],
-    'installable': True,
-    'auto_install': True,
+    "depends": ["stock_barcodes_gs1", "product_expiry",],
+    "data": ["wizard/stock_production_lot_views.xml",],
+    "installable": True,
+    "auto_install": True,
 }
