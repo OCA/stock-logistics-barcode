0c6e68ccdcd9a829e1124bdaea7844a3f463b569
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-07-31 12:16:17 +0200

    [FIX] stock_barcodes_gs1: Pass argument to super
    If not, you got an error.

--- stock_barcodes_gs1/wizard/stock_barcodes_read.py
+++ stock_barcodes_gs1/wizard/stock_barcodes_read.py
@@ -82,4 +82,4 @@ class WizStockBarcodesRead(models.AbstractModel):
             self.action_done()
             self._set_messagge_info("success", _("Barcode read correctly"))
             return True
-        return super().process_barcode()
+        return super().process_barcode(barcode)
