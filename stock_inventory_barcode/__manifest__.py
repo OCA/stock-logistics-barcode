5806bc78313bfc888fa7e54ebf5b737e5ef25c6f
Alexis de Lattre <alexis.delattre@akretion.com>
2020-10-19 00:27:13 +0200

    [MIG] stock_inventory_barcode from v10 to v12
    Add support for multi-stock locations and lots

--- stock_inventory_barcode/__manifest__.py
+++ stock_inventory_barcode/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-# © 2015-2018 Akretion (http://www.akretion.com)
+# Copyright 2015-2020 Akretion France (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': "Barcode Input for Inventories",
-    'version': "10.0.0.1.0",
+    'version': "12.0.1.0.0",
     'category': "Inventory, Logistic, Storage",
     'author': 'Akretion,Odoo Community Association (OCA)',
     'summary': "Add simple barcode interface on inventories",
