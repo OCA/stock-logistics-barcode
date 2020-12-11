20ca90383b1ef3d715f022810f3351e9ecf263c2
Chafique <chafique.delli@akretion.com>
2020-12-11 17:55:14 +0100

    [IMP] barcodes_generator_product: black, isort, prettier

--- barcodes_generator_product/__manifest__.py
+++ barcodes_generator_product/__manifest__.py
@@ -5,28 +5,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Generate Barcodes for Products',
-    'summary': 'Generate Barcodes for Products (Templates and Variants)',
-    'version': '12.0.1.0.0',
-    'category': 'Tools',
-    'author':
-        'GRAP,'
-        'La Louve,'
-        'Odoo Community Association (OCA)',
-    'website': 'https://www.odoo-community.org',
-    'license': 'AGPL-3',
-    'depends': [
-        'barcodes_generator_abstract',
-        'product',
+    "name": "Generate Barcodes for Products",
+    "summary": "Generate Barcodes for Products (Templates and Variants)",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "author": "GRAP," "La Louve," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-barcode",
+    "license": "AGPL-3",
+    "depends": [
+        "barcodes_generator_abstract",
+        "product",
     ],
-    'data': [
-        'views/view_product_product.xml',
-        'views/view_product_template.xml',
+    "data": [
+        "views/view_product_product.xml",
+        "views/view_product_template.xml",
     ],
-    'demo': [
-        'demo/res_users.xml',
-        'demo/barcode_rule.xml',
-        'demo/product.xml',
-        'demo/function.xml',
+    "demo": [
+        "demo/res_users.xml",
+        "demo/barcode_rule.xml",
+        "demo/product.xml",
+        "demo/function.xml",
     ],
 }
