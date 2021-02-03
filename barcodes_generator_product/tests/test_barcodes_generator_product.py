9fffc97734e2832540b7338f0f69491cb68c44df
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-03 12:06:34 +0100

    [IMP] barcodes_generator_product: black, isort, prettier

--- barcodes_generator_product/tests/test_barcodes_generator_product.py
+++ barcodes_generator_product/tests/test_barcodes_generator_product.py
@@ -11,26 +11,36 @@ class Tests(TransactionCase):
 
     def setUp(self):
         super(Tests, self).setUp()
-        self.template_obj = self.env['product.template']
-        self.product_obj = self.env['product.product']
+        self.template_obj = self.env["product.template"]
+        self.product_obj = self.env["product.product"]
 
     # Test Section
     def test_01_manual_generation_template(self):
-        self.template_mono = self.template_obj.browse(self.ref(
-            'barcodes_generator_product.product_template_mono_variant'))
+        self.template_mono = self.template_obj.browse(
+            self.ref("barcodes_generator_product.product_template_mono_variant")
+        )
         self.assertEqual(
-            self.template_mono.barcode, "2000050000003",
+            self.template_mono.barcode,
+            "2000050000003",
             "Incorrect Manual Barcode Generation for non varianted Template."
-            " Pattern : %s - Base : %s" % (
+            " Pattern : %s - Base : %s"
+            % (
                 self.template_mono.barcode_rule_id.pattern,
-                self.template_mono.barcode_base))
+                self.template_mono.barcode_base,
+            ),
+        )
 
     def test_02_manual_generation_product(self):
-        self.product_variant_1 = self.product_obj.browse(self.ref(
-            'barcodes_generator_product.product_product_variant_1'))
+        self.product_variant_1 = self.product_obj.browse(
+            self.ref("barcodes_generator_product.product_product_variant_1")
+        )
         self.assertEqual(
-            self.product_variant_1.barcode, "2010001000006",
+            self.product_variant_1.barcode,
+            "2010001000006",
             "Incorrect Manual Barcode Generation for varianted Product."
-            " Pattern : %s - Base : %s" % (
+            " Pattern : %s - Base : %s"
+            % (
                 self.product_variant_1.barcode_rule_id.pattern,
-                self.product_variant_1.barcode_base))
+                self.product_variant_1.barcode_base,
+            ),
+        )
