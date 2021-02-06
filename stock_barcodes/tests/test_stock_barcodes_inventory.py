48a65cede7e28db2ee35a3af4641aa1d74e4862f
sergio-teruel <sergio.teruel@tecnativa.com>
2021-02-06 21:14:33 +0100

    [ADD] stock_barcodes: Add option to get lots automatically in inventory depends on removal strategy TT27844

--- stock_barcodes/tests/test_stock_barcodes_inventory.py
+++ stock_barcodes/tests/test_stock_barcodes_inventory.py
@@ -81,3 +81,121 @@ class TestStockBarcodesInventory(TestStockBarcodes):
         self.assertEqual(len(self.inventory.line_ids), 1.0)
         self.wiz_scan_inventory.action_undo_last_scan()
         self.assertEqual(self.inventory.line_ids.product_qty, 0.0)
+
+    def testinventory_wizard_scan_product_auto_lot(self):
+        # Prepare more data
+        lot_2 = self.StockProductionLot.create(
+            {
+                "name": "8411822222578",
+                "product_id": self.product_tracking.id,
+                "company_id": self.company.id,
+            }
+        )
+        lot_3 = self.StockProductionLot.create(
+            {
+                "name": "8411822222588",
+                "product_id": self.product_tracking.id,
+                "company_id": self.company.id,
+            }
+        )
+        quant_lot_2 = self.StockQuant.create(
+            {
+                "product_id": self.product_tracking.id,
+                "lot_id": lot_2.id,
+                "location_id": self.stock_location.id,
+                "quantity": 15.0,
+            }
+        )
+        quant_lot_3 = self.StockQuant.create(
+            {
+                "product_id": self.product_tracking.id,
+                "lot_id": lot_3.id,
+                "location_id": self.stock_location.id,
+                "quantity": 10.0,
+            }
+        )
+        self.quant_lot_1.in_date = "2021-01-01"
+        quant_lot_2.in_date = "2021-01-05"
+        quant_lot_3.in_date = "2021-01-06"
+        # Scan product with tracking lot enable
+        self.action_barcode_scanned(self.wiz_scan_inventory, "8433281006850")
+        self.assertEqual(
+            self.wiz_scan_inventory.message,
+            "Barcode: 8433281006850 (Waiting for input lot)",
+        )
+
+        self.wiz_scan_inventory.auto_lot = True
+        self.wiz_scan_inventory.manual_entry = True
+        # Removal strategy FIFO
+        self.action_barcode_scanned(self.wiz_scan_inventory, "8433281006850")
+        self.wiz_scan_inventory.product_qty = 125.0
+        self.wiz_scan_inventory.action_manual_entry()
+        inventory_lines = self.wiz_scan_inventory.inventory_id.line_ids
+        self.assertEqual(inventory_lines[0].product_qty, 100.00)
+        self.assertEqual(inventory_lines[1].product_qty, 15.00)
+        self.assertEqual(inventory_lines[2].product_qty, 10.00)
+
+        # Removal strategy FIFO
+        # Read more quantities than total quants in stock. The extra quantity
+        # should be assigned to last lot.
+        self.wiz_scan_inventory.inventory_id.line_ids.action_reset_product_qty()
+        self.action_barcode_scanned(self.wiz_scan_inventory, "8433281006850")
+        self.wiz_scan_inventory.product_qty = 150.0
+        self.wiz_scan_inventory.action_manual_entry()
+        inventory_lines = self.wiz_scan_inventory.inventory_id.line_ids
+        line_assigned = inventory_lines.filtered(lambda x: x.prod_lot_id == lot_3)
+        self.assertEqual(line_assigned.product_qty, 35.0)
+
+        # Removal strategy LIFO
+        # Read 5.0 quantities and these should be assigned to last lot
+        self.wiz_scan_inventory.inventory_id.line_ids.action_reset_product_qty()
+        self.wiz_scan_inventory.lot_id = False
+        self.product_tracking.categ_id.removal_strategy_id = self.env.ref(
+            "stock.removal_lifo"
+        )
+        self.action_barcode_scanned(self.wiz_scan_inventory, "8433281006850")
+        self.wiz_scan_inventory.product_qty = 5.0
+        self.wiz_scan_inventory.action_manual_entry()
+        inventory_lines = self.wiz_scan_inventory.inventory_id.line_ids
+        line_assigned = inventory_lines.filtered(lambda x: x.product_qty > 0.0)
+        self.assertEqual(line_assigned.prod_lot_id, lot_3)
+        self.assertEqual(line_assigned.product_qty, 5)
+
+        # Barcode read OK
+        self.assertEqual(
+            self.wiz_scan_inventory.message, "Barcode: 8433281006850 (Manual entry OK)"
+        )
+
+        # Remove quants for this product
+        quants = self.StockQuant.search([("product_id", "=", self.product_tracking.id)])
+        quants.unlink()
+        self.wiz_scan_inventory.inventory_id.line_ids.action_reset_product_qty()
+        self.wiz_scan_inventory.lot_id = False
+        self.action_barcode_scanned(self.wiz_scan_inventory, "8433281006850")
+        self.wiz_scan_inventory.product_qty = 5.0
+        self.wiz_scan_inventory.action_manual_entry()
+        self.assertEqual(
+            self.wiz_scan_inventory.message,
+            "Barcode: 8433281006850 (There is no lots to assign quantities)",
+        )
+
+    def test_inventory_wizard_auto_lot_default_value(self):
+        # Company auto lot default value False
+        self.assertFalse(self.wiz_scan_inventory.auto_lot)
+        self.env.user.company_id.stock_barcodes_inventory_auto_lot = True
+        vals = self.inventory.action_barcode_scan()
+        wiz_scan_inventory = self.ScanReadInventory.with_context(
+            vals["context"]
+        ).create({})
+        self.assertTrue(wiz_scan_inventory)
+
+    def test_inventory_wizard_onchanges(self):
+        self.wiz_scan_inventory.lot_id = self.lot_1
+        self.wiz_scan_inventory._onchange_lot_id()
+        self.assertFalse(self.wiz_scan_inventory.auto_lot)
+        self.wiz_scan_inventory.product_id_id = self.product_tracking
+        self.wiz_scan_inventory._onchange_product_id()
+        self.assertEqual(
+            self.wiz_scan_inventory.auto_lot,
+            bool(self.wiz_scan_inventory._default_auto_lot()),
+        )
