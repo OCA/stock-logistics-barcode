61f9f5ddcb51d5cddb27208a9a99a9e6ae10439d
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-08 09:19:43 +0100

    [MIG] barcodes_generator_product: Migration to v13.0

--- barcodes_generator_product/tests/test_barcodes_generator_product.py
+++ barcodes_generator_product/tests/test_barcodes_generator_product.py
@@ -10,15 +10,34 @@ class Tests(TransactionCase):
     """Tests 'Barcodes Generator for Products'"""
 
     def setUp(self):
-        super(Tests, self).setUp()
+        super().setUp()
         self.template_obj = self.env["product.template"]
         self.product_obj = self.env["product.product"]
+        self.barcode_rule = self.env["barcode.rule"].create(
+            {
+                "name": "Product Rule Test",
+                "barcode_nomenclature_id": self.env.ref(
+                    "barcodes.default_barcode_nomenclature"
+                ).id,
+                "type": "product",
+                "sequence": 999,
+                "encoding": "ean13",
+                "pattern": "20.....{NNNDD}",
+                "generate_type": "manual",
+                "generate_model": "product.product",
+            }
+        )
 
     # Test Section
     def test_01_manual_generation_template(self):
-        self.template_mono = self.template_obj.browse(
-            self.ref("barcodes_generator_product.product_template_mono_variant")
+        self.template_mono = self.template_obj.create(
+            {
+                "name": "Template Mono Variant",
+                "barcode_rule_id": self.barcode_rule.id,
+                "barcode_base": 50,
+            }
         )
+        self.template_mono.generate_barcode()
         self.assertEqual(
             self.template_mono.barcode,
             "2000050000003",
@@ -31,9 +50,18 @@ class Tests(TransactionCase):
         )
 
     def test_02_manual_generation_product(self):
-        self.product_variant_1 = self.product_obj.browse(
-            self.ref("barcodes_generator_product.product_product_variant_1")
+        self.template_multi = self.template_obj.create(
+            {"name": "Template Multi Variant"}
         )
+        self.product_variant_1 = self.product_obj.create(
+            {
+                "name": "Variant 1",
+                "product_tmpl_id": self.template_multi.id,
+                "barcode_rule_id": self.barcode_rule.id,
+                "barcode_base": 10001,
+            }
+        )
+        self.product_variant_1.generate_barcode()
         self.assertEqual(
             self.product_variant_1.barcode,
             "2010001000006",
