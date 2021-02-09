5cfd7efb8929c5642657f59243dbd2b0324cb789
sergio-teruel <sergio.teruel@tecnativa.com>
2021-02-09 11:11:31 +0100

    [ADD] stock_barcodes_gs1_secondary_unit: New module

---
+++ stock_barcodes_gs1_secondary_unit/wizard/stock_barcodes_read_picking.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Sergio Teruel <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import models
+
+
+class WizStockBarcodesReadPicking(models.TransientModel):
+    _inherit = "wiz.stock.barcodes.read.picking"
+
+    def _prepare_move_line_values(self, candidate_move, available_qty):
+        vals = super()._prepare_move_line_values(candidate_move, available_qty)
+        vals.update(
+            {
+                "secondary_uom_id": self.secondary_uom_id.id,
+                "secondary_uom_qty": self.secondary_uom_qty,
+            }
+        )
+        return vals
