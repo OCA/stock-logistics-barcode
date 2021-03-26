e315a63d97526883c78e8dc97532ccd682b7996b
Carlos Roca <carlos.roca@tecnativa.com>
2021-03-26 15:47:26 +0100

    [IMP] stock_picking_product_barcode_report: Add the possibility to define other report format

--- stock_picking_product_barcode_report/__manifest__.py
+++ stock_picking_product_barcode_report/__manifest__.py
@@ -17,6 +17,8 @@
         "wizard/stock_barcode_selection_printing_view.xml",
         "report/report_label_barcode.xml",
         "report/report_label_barcode_template.xml",
+        "data/ir_actions_report_data.xml",
+        "views/ir_actions_report_view.xml",
     ],
     "installable": True,
 }
