62531607e79a9353a089d846200c609a09861cde
sergio-teruel <sergio.teruel@tecnativa.com>
2021-04-29 20:52:22 +0200

    [IMP] stock_barcode: Tests

--- stock_barcodes/tests/test_stock_barcodes.py
+++ stock_barcodes/tests/test_stock_barcodes.py
@@ -146,8 +146,7 @@ class TestStockBarcodes(TransactionCase):
         self.product_wo_tracking.packaging_ids.barcode = "5420008510489"
         self.action_barcode_scanned(self.wiz_scan, "5420008510489")
         self.assertEqual(
-            self.wiz_scan.message,
-            "Barcode: 5420008510489 (More than one package found)",
+            self.wiz_scan.message, "5420008510489 (More than one package found)",
         )
 
     def test_wizard_scan_lot(self):
@@ -163,20 +162,10 @@ class TestStockBarcodes(TransactionCase):
 
     def test_wizard_scan_not_found(self):
         self.action_barcode_scanned(self.wiz_scan, "84118xxx22568")
-        self.assertEqual(
-            self.wiz_scan.message, "Barcode: 84118xxx22568 (Barcode not found)"
-        )
+        self.assertEqual(self.wiz_scan.message, "84118xxx22568 (Barcode not found)")
 
     def test_wizard_remove_last_scan(self):
         self.assertTrue(self.wiz_scan.action_undo_last_scan())
-
-    def test_wizard_onchange_location(self):
-        self.action_barcode_scanned(self.wiz_scan, "8480000723208")
-        self.assertEqual(self.wiz_scan.product_id, self.product_wo_tracking)
-        self.wiz_scan.location_id = self.location_2
-        self.wiz_scan.onchange_location_id()
-        self.assertFalse(self.wiz_scan.product_id)
-        self.assertFalse(self.wiz_scan.packaging_id)
 
     def test_wiz_clean_lot(self):
         self.action_barcode_scanned(self.wiz_scan, "8433281006850")
