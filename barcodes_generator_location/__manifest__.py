2cf6ff8e1c69d7018e0e723c68da2453c1e852d4
Zar21 <ivanperezfita@gmail.com>
2021-05-18 16:54:36 +0200

    [MIG] barcodes_generator_location: Migration to 14.0

--- barcodes_generator_location/__manifest__.py
+++ barcodes_generator_location/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Generate Barcodes for Stock Locations",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Tools",
     "author": "LasLabs, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-barcode",
