2802855c39f5f46789f631e9d44f90114f092a02
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-06-15 17:44:39 -0300

    [FIX] product_multi_barcode: installation hook

--- product_multi_barcode/hooks.py
+++ product_multi_barcode/hooks.py
@@ -8,8 +8,8 @@ def post_init_hook(cr, registry):
     cr.execute(
         """
     INSERT INTO product_barcode
-    (product_id, name, sequence)
-    SELECT id, barcode, 0
+    (product_id, product_tmpl_id, name, sequence)
+    SELECT id, product_tmpl_id, barcode, 0
     FROM product_product
     WHERE barcode IS NOT NULL"""
     )
