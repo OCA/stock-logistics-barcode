f6f0db143598b83fc8d40694a991258cd2a12aec
oca-travis <oca+oca-travis@odoo-community.org>
2021-06-17 12:01:16 +0000

    [UPD] Update base_gs1_barcode.pot

--- base_gs1_barcode/__manifest__.py
+++ base_gs1_barcode/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "GS1 Barcode API",
     "summary": "Decoding API for GS1-128 (aka UCC/EAN-128) and GS1-Datamatrix",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "Numérigraphe, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-barcode",
     "category": "Generic Modules/Inventory Control",
