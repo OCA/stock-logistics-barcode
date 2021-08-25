1b64392dd0104097ae3d991c4a442d6629cbaf88
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-25 18:05:20 +0200

    stock_inventory_barcode: black, isort, prettier, etc.

--- stock_inventory_barcode/__manifest__.py
+++ stock_inventory_barcode/__manifest__.py
@@ -3,18 +3,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "Barcode Input for Inventories",
-    'version': "14.0.1.0.0",
-    'category': "Inventory, Logistic, Storage",
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'maintainers': ['alexis-via'],
-    'summary': "Add simple barcode interface on inventories",
-    'license': 'AGPL-3',
-    'depends': ['stock'],
-    'data': [
-        'security/ir.model.access.csv',
-        'wizard/stock_inventory_barcode_view.xml',
-        'views/stock_inventory.xml',
-        ],
-    'installable': True,
+    "name": "Barcode Input for Inventories",
+    "version": "14.0.1.0.0",
+    "category": "Inventory, Logistic, Storage",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "summary": "Add simple barcode interface on inventories",
+    "website": "https://github.com/OCA/stock-logistics-barcode",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/stock_inventory_barcode_view.xml",
+        "views/stock_inventory.xml",
+    ],
+    "installable": True,
 }
