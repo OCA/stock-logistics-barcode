2d5913a8a725cce3eaa1dd636990b206527f15ca
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2022-02-17 11:23:10 +0100

    [IMP] stock_barcodes_gs1_expiry: black, isort, prettier

--- stock_barcodes_gs1_expiry/__manifest__.py
+++ stock_barcodes_gs1_expiry/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "It provides read expiry dates from GS1 barcode on " "stock operations.",
     "version": "13.0.1.0.0",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
-    "website": "https://odoo-community.org/",
+    "website": "https://github.com/OCA/stock-logistics-barcode",
     "license": "AGPL-3",
     "category": "Extra Tools",
     "depends": ["stock_barcodes_gs1", "product_expiry"],
