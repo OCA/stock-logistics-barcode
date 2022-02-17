ac6a29109e832386df3e842d72d5e8fe3ddaa2ec
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2022-02-17 11:30:10 +0100

    [MIG] stock_barcodes_gs1_expiry: Migration to 14.0

--- stock_barcodes_gs1_expiry/wizard/stock_barcodes_read.py
+++ stock_barcodes_gs1_expiry/wizard/stock_barcodes_read.py
@@ -6,7 +6,7 @@ from odoo import fields, models
 class WizStockBarcodesRead(models.AbstractModel):
     _inherit = "wiz.stock.barcodes.read"
 
-    life_date = fields.Datetime(
+    expiration_date = fields.Datetime(
         string="End of Life Date",
     )
     use_date = fields.Datetime(
@@ -16,9 +16,9 @@ class WizStockBarcodesRead(models.AbstractModel):
     def _prepare_lot_values(self, barcode_decoded):
         res = super()._prepare_lot_values(barcode_decoded)
         preferred_date = barcode_decoded.get("15", False)
-        life_date = barcode_decoded.get("17", False)
+        expiration_date = barcode_decoded.get("17", False)
         if preferred_date:
             res["use_date"] = preferred_date
-        if life_date:
-            res["life_date"] = life_date
+        if expiration_date:
+            res["expiration_date"] = expiration_date
         return res
