a18b49f892c194c4ac31242e12884aa38498cd3f
David Beal <david.beal@akretion.com>
2022-03-21 09:40:34 +0100

    ADD module sale_input_barcode

---
+++ sale_input_barcode/tests/test_module.py
@@ -0,0 +1,23 @@
+# © 2022 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests import TransactionCase
+
+
+class Test(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_input_line(self):
+        self.env = self.env(context=dict(self.env.context, tracking_disable=True))
+        barcode = "01034009338167591714050010B04059A\x1d310500638515140501"
+        product = self.env["product.product"].create(
+            {
+                "name": "barcode test",
+                "barcode": barcode[2:16],
+            }
+        )
+        sale = self.env.ref("sale.sale_order_3")
+        sale.action_sale_line_barcode(barcode)
+        self.assertIn(product, sale.order_line.mapped("product_id"))
