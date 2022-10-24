17162cefd2d5ef0fad6ff23950693d12b2b1c3e2
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-10-25 00:42:49 +0200

    [MIG] barcodes_generator_product from 14.0 to 16.0

--- barcodes_generator_product/__manifest__.py
+++ barcodes_generator_product/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Generate Barcodes for Products",
     "summary": "Generate Barcodes for Products (Templates and Variants)",
-    "version": "14.0.1.1.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "author": "GRAP, La Louve, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-barcode",
@@ -19,6 +19,5 @@
         "demo/res_users.xml",
         "demo/barcode_rule.xml",
         "demo/product.xml",
-        "demo/function.xml",
     ],
 }
--- barcodes_generator_product/models/product_template.py
+++ barcodes_generator_product/models/product_template.py
@@ -11,22 +11,22 @@ class ProductTemplate(models.Model):
 
     # Related to display product product information if is_product_variant
     barcode_rule_id = fields.Many2one(
-        related="product_variant_ids.barcode_rule_id",
         string="Barcode Rule",
-        readonly=False,
+        related="product_variant_ids.barcode_rule_id",
         comodel_name="barcode.rule",
+        readonly=False,
     )
 
     barcode_base = fields.Integer(
+        string="Barcode Base",
         related="product_variant_ids.barcode_base",
         readonly=False,
-        string="Barcode Base",
     )
 
     generate_type = fields.Selection(
         string="Generate Type",
-        readonly=True,
         related="product_variant_ids.barcode_rule_id.generate_type",
+        readonly=True,
     )
 
     # View Section
@@ -42,17 +42,19 @@ class ProductTemplate(models.Model):
         self.generate_type = self.barcode_rule_id.generate_type
 
     # Overload Section
-    @api.model
-    def create(self, vals):
-        template = super().create(vals)
-
+    @api.model_create_multi
+    def create(self, vals_list):
         # this is needed to set given values to first variant after creation
         # these fields should be moved to product as lead to confusion
         # (Ref. product module feature in Odoo Core)
-        related_vals = {}
-        for field in ["barcode_rule_id", "barcode_base"]:
-            if vals.get(field, False):
-                related_vals[field] = vals[field]
-        if related_vals:
-            template.write(related_vals)
-        return template
+        result = self
+        for vals in vals_list:
+            template = super().create(vals)
+            related_vals = {}
+            for field in ["barcode_rule_id", "barcode_base"]:
+                if vals.get(field, False):
+                    related_vals[field] = vals[field]
+            if related_vals:
+                template.write(related_vals)
+            result |= template
+        return result
