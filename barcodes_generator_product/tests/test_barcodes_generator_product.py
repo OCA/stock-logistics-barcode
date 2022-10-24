958135253cc13801ae8056ab89888c227a2e7379
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-10-25 00:42:49 +0200

    [MIG] barcodes_generator_product from 14.0 to 16.0

--- barcodes_generator_product/tests/test_barcodes_generator_product.py
+++ barcodes_generator_product/tests/test_barcodes_generator_product.py
@@ -11,36 +11,25 @@ class Tests(TransactionCase):
 
     def setUp(self):
         super().setUp()
-        self.template_obj = self.env["product.template"]
-        self.product_obj = self.env["product.product"]
-        self.barcode_rule = self.env["barcode.rule"].create(
-            {
-                "name": "Product Rule Test",
-                "barcode_nomenclature_id": self.env.ref(
-                    "barcodes.default_barcode_nomenclature"
-                ).id,
-                "type": "product",
-                "sequence": 999,
-                "encoding": "ean13",
-                "pattern": "20.....{NNNDD}",
-                "generate_type": "manual",
-                "generate_model": "product.product",
-            }
+        self.ProductTemplate = self.env["product.template"]
+        self.ProductProduct = self.env["product.product"]
+        self.barcode_rule_manually = self.env.ref(
+            "barcodes_generator_product.rule_product_generated_barcode_manually"
         )
 
     # Test Section
     def test_01_manual_generation_template(self):
-        self.template_mono = self.template_obj.create(
+        self.template_mono = self.ProductTemplate.create(
             {
                 "name": "Template Mono Variant",
-                "barcode_rule_id": self.barcode_rule.id,
-                "barcode_base": 50,
+                "barcode_rule_id": self.barcode_rule_manually.id,
+                "barcode_base": 54321,
             }
         )
         self.template_mono.generate_barcode()
         self.assertEqual(
             self.template_mono.barcode,
-            "2000050000003",
+            "2054321000001",
             "Incorrect Manual Barcode Generation for non varianted Template."
             " Pattern : %s - Base : %s"
             % (
@@ -50,21 +39,21 @@ class Tests(TransactionCase):
         )
 
     def test_02_manual_generation_product(self):
-        self.template_multi = self.template_obj.create(
+        self.template_multi = self.ProductTemplate.create(
             {"name": "Template Multi Variant"}
         )
-        self.product_variant_1 = self.product_obj.create(
+        self.product_variant_1 = self.ProductProduct.create(
             {
                 "name": "Variant 1",
                 "product_tmpl_id": self.template_multi.id,
-                "barcode_rule_id": self.barcode_rule.id,
-                "barcode_base": 10001,
+                "barcode_rule_id": self.barcode_rule_manually.id,
+                "barcode_base": 12345,
             }
         )
         self.product_variant_1.generate_barcode()
         self.assertEqual(
             self.product_variant_1.barcode,
-            "2010001000006",
+            "2012345000001",
             "Incorrect Manual Barcode Generation for varianted Product."
             " Pattern : %s - Base : %s"
             % (
