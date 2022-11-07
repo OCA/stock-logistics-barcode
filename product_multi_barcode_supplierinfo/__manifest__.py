93fc47dd16e441cd65432fc722503723b152092c
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-11-07 12:38:15 +0100

    [ADD] product_multi_barcode_supplierinfo

---
+++ product_multi_barcode_supplierinfo/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Multi barcode from supplier pricelist",
+    "summary": "Adds  supplier pricelist barcode in product barcode",
+    "version": "14.0.1.0.0",
+    "category": "Product Management",
+    "website": "https://github.com/OCA/stock-logistics-barcode",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["product_supplierinfo_barcode", "product_multi_barcode"],
+    "installable": True,
+    "auto_install": True,
+    "data": [
+        "views/product_template_view.xml",
+    ],
+    "pre_init_hook": "copy_barcode",
+    "post_init_hook": "update_barcodes_ids",
+}
---
+++ product_multi_barcode_supplierinfo/models/supplierinfo.py
@@ -0,0 +1,66 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class SupplierInfo(models.Model):
+    _inherit = "product.supplierinfo"
+
+    barcode_id = fields.Many2one(comodel_name="product.barcode")
+
+    barcode = fields.Char(
+        compute="_compute_barcode_name",
+        inverse="_inverse_barcode_field",
+        readonly=False,
+        store=True,
+    )
+
+    @api.depends("barcode_id.name")
+    def _compute_barcode_name(self):
+        for rec in self:
+            if rec.barcode_id:
+                rec.barcode = rec.barcode_id.name
+
+    def _inverse_barcode_field(self):
+        barcode = self.env["product.barcode"].search([("name", "=", self.barcode)])
+        if not self.barcode_id:
+            if not barcode:
+                self.barcode_id = self.barcode_id.create({"name": self.barcode})
+            if barcode:
+                self.barcode_id = barcode
+        else:
+            self.barcode_id.name = self.barcode
+        self.barcode_id.supplier_id = self.name
+        self.barcode_id.product_tmpl_id = self.product_tmpl_id
+        if self.product_tmpl_id.product_variant_count > 1:
+            self.barcode_id.product_id = self.product_id
+
+    @api.constrains("barcode")
+    def check_barcode(self):
+        for rec in self:
+            if (
+                rec.product_tmpl_id.product_variant_count > 1
+                and rec.barcode
+                and (not rec.product_id)
+            ):
+                raise ValidationError(
+                    _("You have to specify variant for the supplierinfo with barcode")
+                )
+
+    def unlink_product_barcode(self):
+        product_barcode_id = self.env["product.barcode"].search(
+            [
+                ("product_tmpl_id", "=", self.product_tmpl_id.id),
+                ("name", "=", self.barcode_id.name),
+            ]
+        )
+        if product_barcode_id:
+            product_barcode_id.unlink()
+
+    def unlink(self):
+        for rec in self:
+            if rec.barcode:
+                rec.unlink_product_barcode()
+        super().unlink()
