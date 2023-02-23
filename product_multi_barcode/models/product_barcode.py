9517569259aaa625364c3a8e4d856d5e8100a587
Sébastien BEAU <sebastien.beau@akretion.com>
2023-02-23 09:54:22 +0100

    product_multi_barcode: fix muti-company access right issue

--- product_multi_barcode/models/product_barcode.py
+++ product_multi_barcode/models/product_barcode.py
@@ -56,7 +56,19 @@ class ProductBarcode(models.Model):
                 [("id", "!=", record.id), ("name", "=", record.name)]
             )
             if barcodes:
+                # by default barcode 'shared' between all company (no ir.rule)
+                # so we may not have the access right on the product
+                # note: if you do not want to share the barcode between company
+                # you just need to add a custom ir.rule
+                product = barcodes[0].sudo().product_id
                 raise UserError(
-                    _('The Barcode "%s" already exists for product ' '"%s"')
-                    % (record.name, barcodes[0].product_id.name)
+                    _(
+                        'The Barcode "%(barcode_name)s" already exists for '
+                        'product "%(product_name)s" in the company %(company_name)s'
+                    )
+                    % dict(
+                        barcode_name=record.name,
+                        product_name=product.name,
+                        company_name=product.company_id.name,
+                    )
                 )
