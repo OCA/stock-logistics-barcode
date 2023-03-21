8d615567e556c9184248404b2cfa7cb40d631add
Raul-S73 <raul@studio73.es>
2023-03-21 15:08:58 +0100

    [MIG] barcode_generator_package: migration to 14.0

--- barcodes_generator_package/tests/test_barcode_generator.py
+++ barcodes_generator_package/tests/test_barcode_generator.py
@@ -1,27 +1,27 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import SavepointCase
 
 
-class TestBarcodeGenerator(TransactionCase):
-
-    def setUp(self):
-        super(TestBarcodeGenerator, self).setUp()
-        self.record = self.env.ref(
-            'barcodes_generator_package.product_packaging_barcode',
+class TestBarcodeGenerator(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestBarcodeGenerator, cls).setUpClass()
+        cls.record = cls.env.ref(
+            "barcodes_generator_package.product_packaging_barcode",
         )
-        self.record.generate_barcode()
+        cls.record.generate_barcode()
 
     def test_generate_base(self):
-        """ It should generate the correct base for the barcode. """
+        """It should generate the correct base for the barcode."""
         self.assertEqual(
-            self.record.barcode_base, 1,
+            self.record.barcode_base,
+            1,
         )
 
     def test_generate_sequence(self):
-        """ It should generate the correct sequence for the barcode. """
+        """It should generate the correct sequence for the barcode."""
         self.assertEqual(
             self.record.barcode,
             "1230000000017",
