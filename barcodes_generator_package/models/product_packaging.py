f4fdc6f6baf82c00995500bea2155b40111dce00
Raul-S73 <raul@studio73.es>
2023-03-21 15:08:58 +0100

    [MIG] barcode_generator_package: migration to 14.0

--- barcodes_generator_package/models/product_packaging.py
+++ barcodes_generator_package/models/product_packaging.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -6,8 +5,8 @@ from odoo import fields, models
 
 
 class ProductPackaging(models.Model):
-    _name = 'product.packaging'
-    _description = 'Product Packaging'
-    _inherit = ['product.packaging', 'barcode.generate.mixin']
+    _name = "product.packaging"
+    _description = "Product Packaging"
+    _inherit = ["product.packaging", "barcode.generate.mixin"]
 
     barcode = fields.Char()
---
+++ barcodes_generator_package/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Dave Lasley <dave@laslabs.com>
+* `Studio73 <https://www.studio73.es>`__:
+    * Raúl Menéndez <raul@studio73.es>
