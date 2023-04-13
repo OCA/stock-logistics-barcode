fedd790469db36bf604bd73322b8eb127cf27d7c
Pierrick Brun <pierrick.brun@akretion.com>
2023-04-13 15:10:00 +0200

    [ADD] product_multi_barcode_constraint_per_company

---
+++ product_multi_barcode_constraint_per_company/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Pierrick Brun <pierrick.brun@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Multi Barcode Constraint per Company",
+    "version": "16.0.1.0.0",
+    "category": "Product",
+    "summary": "Glue module for product_multi_barcode and"
+    "product_barcode_constraint_per_company",
+    "author": "akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-barcode",
+    "license": "AGPL-3",
+    "depends": [
+        "product_multi_barcode",
+        "product_barcode_constraint_per_company",
+    ],
+    "data": ["security/ir_rule.xml"],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ product_multi_barcode_constraint_per_company/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This is a glue module for product_multi_barcode and
+product_multi_barcode_constraint_per_company
+
+It allows to make barcodes company dependent while using multiple barcodes per products
