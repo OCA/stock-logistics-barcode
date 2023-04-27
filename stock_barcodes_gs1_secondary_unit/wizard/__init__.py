cc5045992f6a657218c8020d48a8109d0ed020be
sergio-teruel <sergio.teruel@tecnativa.com>
2023-04-27 19:50:56 +0200

    [IMP] stock_barcodes_gs1_secondary_unit: more secondary uom info in barcodes interface

--- stock_barcodes_gs1_secondary_unit/wizard/__init__.py
+++ stock_barcodes_gs1_secondary_unit/wizard/__init__.py
@@ -2,3 +2,4 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import stock_barcodes_read
 from . import stock_barcodes_read_picking
+from . import stock_barcodes_read_todo
