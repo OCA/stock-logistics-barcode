149315fc83acf58e07d34c69ebd2b295637441db
sergio-teruel <sergio.teruel@tecnativa.com>
2023-05-10 00:01:47 +0200

    [IMP] stock_barcodes: Allow display detailed operations in barcode wizard

--- stock_barcodes/models/stock_move_line.py
+++ stock_barcodes/models/stock_move_line.py
@@ -22,3 +22,15 @@ class StockMoveLine(models.Model):
                 line.barcode_scan_state = "done"
             else:
                 line.barcode_scan_state = "pending"
+
+    def action_barcode_detailed_operation_unlink(self):
+        for sml in self:
+            if sml.product_uom_qty:
+                sml.qty_done = 0.0
+            else:
+                sml.unlink()
+            wiz_barcode = self.env["wiz.stock.barcodes.read.picking"].browse(
+                self.env.context.get("wiz_barcode_id", False)
+            )
+            if wiz_barcode.option_group_id.show_pending_moves:
+                wiz_barcode.fill_todo_records()
