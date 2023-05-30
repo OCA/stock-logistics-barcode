fb47dfe11dbfb579f0940b0a1f9bbe20521cc54b
Carlos Roca <carlos.roca@tecnativa.com>
2023-05-30 12:07:21 +0200

    [IMP] stock_picking_product_barcode_report: Posibility to print labels from HTML field

--- stock_picking_product_barcode_report/__manifest__.py
+++ stock_picking_product_barcode_report/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Picking Product Barcode Report",
     "summary": "It provides a wizard to select how many barcodes print.",
-    "version": "15.0.1.1.0",
+    "version": "15.0.2.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-barcode",
     "license": "AGPL-3",
@@ -19,6 +19,7 @@
         "report/report_label_barcode.xml",
         "report/report_label_barcode_quant_package_template.xml",
         "report/report_label_barcode_template.xml",
+        "report/report_label_custom.xml",
         "views/ir_actions_report_view.xml",
     ],
     "installable": True,
