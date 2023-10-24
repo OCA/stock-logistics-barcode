05725a9ec54f6172defc73170e5e5986983befc0
Telmo Santos <telmo.santos@camptocamp.com>
2023-10-24 14:30:30 +0200

    [IMP] product_packaging_multi_barcode barcode product show packaging

--- product_packaging_multi_barcode/__manifest__.py
+++ product_packaging_multi_barcode/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Multiple barcodes on product packagings",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "license": "AGPL-3",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "category": "Product Management",
@@ -10,6 +10,7 @@
     "website": "https://github.com/OCA/stock-logistics-barcode",
     "data": [
         "views/product_packaging_view.xml",
+        "views/product_template_view.xml",
     ],
     "installable": True,
     "post_init_hook": "post_init_hook",
