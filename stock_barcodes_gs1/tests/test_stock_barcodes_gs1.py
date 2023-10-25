5b9a92638ad76f2883f0bd6436261fa8ee497d14
sergio-teruel <sergio.teruel@tecnativa.com>
2023-10-25 18:11:14 +0200

    [IMP] stock_barcodes_gs1: Refactoring. Use Odoo core module to decode GS1 barcodes. Better process of AI's

--- stock_barcodes_gs1/tests/test_stock_barcodes_gs1.py
+++ stock_barcodes_gs1/tests/test_stock_barcodes_gs1.py
@@ -9,38 +9,46 @@ from odoo.addons.stock_barcodes.tests.test_stock_barcodes import TestStockBarcod
 @common.tagged("post_install", "-at_install")
 class TestStockBarcodesGS1(TestStockBarcodes):
     # pylint: disable=W8121
-    def setUp(self):
-        super().setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        gs1_nomenclature = cls.env.ref(
+            "barcodes_gs1_nomenclature.default_gs1_nomenclature"
+        )
         # Barcode for packaging and lot
-        self.gs1_barcode_01_product = "0119501101530000"
-        self.gs1_barcode_01_lot = "1714070410AB-123"
-        self.gs1_separator = self.env.user.gs1_barcode_separator or "\x1D"
-        self.gs1_barcode_unit_01 = self.gs1_separator + "371"
-        self.gs1_barcode_unit_02 = self.gs1_separator + "372"
-        self.gs1_barcode_01 = self.gs1_barcode_01_product + self.gs1_barcode_01_lot
-        self.gs1_barcode_01_not_found = "011xxx11015300001714070410AB-123"
-        self.gs1_barcode_01_not_lot = "01195011015300001714070410AB-124"
+        cls.gs1_barcode_01_product = "0119501101530000"
+        cls.gs1_barcode_01_lot = "1714070410AB-123"
+        cls.gs1_separator = gs1_nomenclature.gs1_separator_fnc1 or "\x1D"
+        cls.gs1_barcode_unit_01 = cls.gs1_separator + "301"
+        cls.gs1_barcode_unit_02 = cls.gs1_separator + "302"
+        cls.gs1_barcode_unit_03 = cls.gs1_separator + "373"
+        cls.gs1_barcode_01 = cls.gs1_barcode_01_product + cls.gs1_barcode_01_lot
+        cls.gs1_barcode_01_not_found = "011xxx11015300001714070410AB-123"
+        cls.gs1_barcode_01_not_lot = "01195011015300001714070410AB-124"
         # Barcode for product and quantities
-        self.gs1_barcode_02 = "0207010001234567150410183724"
-        self.gs1_barcode_02_not_found = "020xxx0001234567150410183724"
+        cls.gs1_barcode_02 = "0207010001234567150410183724"
+        cls.gs1_barcode_02_not_found = "020xxx0001234567150410183724"
         # Barcode not processed
-        self.gs1_barcode_01_not_processed = (
+        cls.gs1_barcode_01_not_processed = (
             "01993167101234533101002620130" "5041710ABC123214145354"
         )
-        self.product_wo_tracking_gs1 = self.product_wo_tracking.with_context({}).copy(
-            {"barcode": "07010001234567"}
+        cls.product_wo_tracking_gs1 = cls.product_wo_tracking.with_context({}).copy(
+            {"barcode": "07010001234567", "name": "product_wo_tracking_gs1"}
         )
-        self.product_tracking_gs1 = self.product_tracking.with_context({}).copy()
-        self.packaging_gs1 = self.ProductPackaging.create(
+        cls.product_tracking_gs1 = cls.product_tracking.with_context({}).copy(
+            {"name": "product_tracking_gs1"}
+        )
+        cls.packaging_gs1 = cls.ProductPackaging.create(
             {
-                "product_id": self.product_wo_tracking_gs1.id,
+                "product_id": cls.product_wo_tracking_gs1.id,
                 "name": "Box 10 Units",
                 "qty": 10.0,
                 "barcode": "19501101530000",
             }
         )
         # Set location to avoid crash tests
-        self.wiz_scan.location_id = self.location_1
+        cls.wiz_scan.location_id = cls.location_1
 
     def test_wizard_scan_gs1_package_multi(self):
         self.packaging_gs1.product_id = self.product_tracking_gs1
@@ -69,33 +77,42 @@ class TestStockBarcodesGS1(TestStockBarcodes):
         self.assertEqual(self.wiz_scan.packaging_id, self.packaging_gs1)
         # Scan no exist packaging
         self.action_barcode_scanned(self.wiz_scan, self.gs1_barcode_01_not_found)
-        self.assertEqual(
-            self.wiz_scan.message,
+        self.assertIn(
             "011xxx11015300001714070410AB-123 "
-            "(Barcode for product packaging not found)",
+            "(Barcode not found with this screen values)",
+            self.wiz_scan.message,
         )
         # Scan packaging barcode with more than one package
         self.packaging_gs1.with_context({}).copy({"barcode": "19501101530000"})
         self.action_barcode_scanned(self.wiz_scan, self.gs1_barcode_01)
-        self.assertEqual(
-            self.wiz_scan.message,
-            "01195011015300001714070410AB-123 " "(More than one package found)",
+        self.assertIn(
+            "19501101530000 (More than one package found)", self.wiz_scan.message
         )
+        self.assertIn("(10)AB-123 Not found", self.wiz_scan.message)
 
     def test_wizard_scan_gs1_package_units(self):
+        self.packaging_gs1.product_id = self.product_tracking_gs1
+        # Test AI (01) with AI (30)
         self.action_barcode_scanned(self.wiz_scan, self.gs1_barcode_01)
         self.assertEqual(self.wiz_scan.packaging_id, self.packaging_gs1)
         self.assertEqual(self.wiz_scan.product_qty, 10)
+        # Test AI (01) with AI (30)
         self.action_barcode_scanned(
             self.wiz_scan, self.gs1_barcode_01 + self.gs1_barcode_unit_01
         )
         self.assertEqual(self.wiz_scan.packaging_id, self.packaging_gs1)
         self.assertEqual(self.wiz_scan.product_qty, 10)
+        # Test AI (01) with AI (30)
         self.action_barcode_scanned(
             self.wiz_scan, self.gs1_barcode_01 + self.gs1_barcode_unit_02
         )
         self.assertEqual(self.wiz_scan.packaging_id, self.packaging_gs1)
         self.assertEqual(self.wiz_scan.product_qty, 20)
+        # Test AI (02) with AI (37)
+        self.action_barcode_scanned(
+            self.wiz_scan, self.gs1_barcode_02 + self.gs1_barcode_unit_03
+        )
+        self.assertEqual(self.wiz_scan.product_qty, 3)
 
     def test_wizard_scan_gs1_product(self):
         self.wiz_scan.location_id = self.location_1
@@ -104,18 +121,17 @@ class TestStockBarcodesGS1(TestStockBarcodes):
         self.assertEqual(self.wiz_scan.product_qty, 24)
         # Scan non exists product
         self.action_barcode_scanned(self.wiz_scan, self.gs1_barcode_02_not_found)
-        self.assertEqual(
+        self.assertIn(
+            "020xxx0001234567150410183724 (Barcode not found with this screen values)",
             self.wiz_scan.message,
-            "020xxx0001234567150410183724 (Barcode for product packaging not found)",
         )
 
     def test_wizard_scan_gs1_product_as_packaging(self):
+        self.wiz_scan.location_id = self.location_1.id
+        self.wiz_scan.action_show_step()
         self.product_wo_tracking_gs1.barcode = "X07010001234567Xg"
         self.action_barcode_scanned(self.wiz_scan, self.gs1_barcode_02)
-        self.assertEqual(
-            self.wiz_scan.message,
-            "0207010001234567150410183724 " "(Barcode for product packaging not found)",
-        )
+        self.assertIn("(02)07010001234567 Not found", self.wiz_scan.message)
         self.ProductPackaging.create(
             {
                 "product_id": self.product_wo_tracking_gs1.id,
@@ -126,7 +142,7 @@ class TestStockBarcodesGS1(TestStockBarcodes):
         )
         self.action_barcode_scanned(self.wiz_scan, self.gs1_barcode_02)
         self.assertEqual(self.wiz_scan.product_id, self.product_wo_tracking_gs1)
-        self.assertEqual(self.wiz_scan.product_qty, 48)
+        self.assertEqual(self.wiz_scan.product_qty, 24)
 
     def test_wizard_scan_gs1_lot(self):
         self.packaging_gs1.product_id = self.product_tracking_gs1
@@ -139,18 +155,113 @@ class TestStockBarcodesGS1(TestStockBarcodes):
         )
         self.action_barcode_scanned(self.wiz_scan, self.gs1_barcode_01)
         self.assertEqual(self.wiz_scan.lot_id, lot)
-        self.assertEqual(
-            self.wiz_scan.message,
-            "01195011015300001714070410AB-123 " "(Barcode read correctly)",
-        )
         self.action_barcode_scanned(self.wiz_scan, self.gs1_barcode_01_not_lot)
-        self.assertEqual(self.wiz_scan.lot_id.name, "AB-124")
+        self.assertEqual(self.wiz_scan.lot_name, "AB-124")
 
     def test_wizard_scan_gs1_not_found(self):
+        self.wiz_scan.location_id = self.location_1.id
+        self.wiz_scan.action_show_step()
         self.action_barcode_scanned(self.wiz_scan, self.gs1_barcode_01_not_processed)
-        self.assertEqual(
-            self.wiz_scan.message,
-            "0199316710123453310100262013050417"
-            "10ABC123214145354 (Barcode for product packaging "
-            "not found)",
+        self.assertIn("(01)99316710123453 Not found", self.wiz_scan.message)
+        self.assertIn("(10)ABC123214145354 Not found", self.wiz_scan.message)
+
+    def test_wizard_scan_gs1_multi_barcode(self):
+        # Create a packaging for product
+        product = self.product_tracking.with_context({}).copy()
+        packaging_box_24 = self.ProductPackaging.create(
+            {
+                "product_id": product.id,
+                "name": "Box 24 Units",
+                "qty": 1,
+                "barcode": "08412598002106",
+            }
         )
+        packaging_pallet_6 = self.ProductPackaging.create(
+            {
+                "product_id": product.id,
+                "name": "Pallet 60 Units",
+                "qty": 60,
+                "barcode": "384125980801041268",
+            }
+        )
+
+        option_group = self.env["stock.barcodes.option.group"].create(
+            {
+                "name": "option group for tests IN GS1",
+                "create_lot": True,
+                "is_manual_confirm": True,
+                "option_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "step": 1,
+                            "name": "Location",
+                            "field_name": "location_id",
+                            "filled_default": True,
+                            "to_scan": True,
+                            "required": True,
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "step": 2,
+                            "name": "Product",
+                            "field_name": "product_id",
+                            "to_scan": True,
+                            "required": True,
+                            "clean_after_done": True,
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "step": 2,
+                            "name": "Packaging",
+                            "field_name": "packaging_id",
+                            "to_scan": True,
+                            "required": False,
+                            "clean_after_done": True,
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "step": 2,
+                            "name": "Lot / Serial",
+                            "field_name": "lot_id",
+                            "to_scan": True,
+                            "required": True,
+                            "clean_after_done": True,
+                        },
+                    ),
+                ],
+            }
+        )
+
+        wiz_inventory = self.env["wiz.stock.barcodes.read.inventory"].create(
+            {
+                "location_id": self.env["stock.warehouse"]
+                .search([])[:1]
+                .lot_stock_id.id,
+                "option_group_id": option_group.id,
+                "step": 1,
+            }
+        )
+        self.action_barcode_scanned(wiz_inventory, "010841259800210615240914")
+        self.assertEqual(wiz_inventory.packaging_id, packaging_box_24)
+        self.assertEqual(wiz_inventory.product_qty, 1.0)
+
+        self.action_barcode_scanned(wiz_inventory, "0038412598080104126810LOTEG01")
+        self.assertEqual(wiz_inventory.packaging_id, packaging_pallet_6)
+        self.assertEqual(wiz_inventory.product_qty, 60.0)
+
+        self.assertEqual(wiz_inventory.product_id, product)
+        self.assertEqual(wiz_inventory.lot_name, "LOTEG01")
+
+        wiz_inventory.action_confirm()
+        self.assertEqual(wiz_inventory.inventory_quant_ids.lot_id.name, "LOTEG01")
--- stock_barcodes_gs1/wizard/stock_barcodes_read.py
+++ stock_barcodes_gs1/wizard/stock_barcodes_read.py
@@ -6,104 +6,104 @@ from odoo import _, models
 class WizStockBarcodesRead(models.AbstractModel):
     _inherit = "wiz.stock.barcodes.read"
 
-    def _prepare_lot_values(self, barcode_decoded):
-        lot_barcode = barcode_decoded.get("10", False)
-        return {
-            "name": lot_barcode,
-            "product_id": self.product_id.id,
-            "company_id": self.env.company.id,
-        }
-
-    def _create_lot(self, barcode_decoded):
-        return self.env["stock.production.lot"].create(
-            self._prepare_lot_values(barcode_decoded)
-        )
-
-    def process_lot(self, barcode_decoded):
-        lot_barcode = barcode_decoded.get("10", False)
-        lot = self.env["stock.production.lot"].search(
-            [("name", "=", lot_barcode), ("product_id", "=", self.product_id.id)]
-        )
-        if not lot and self.option_group_id.create_lot:
-            lot = self._create_lot(barcode_decoded)
-        self.lot_id = lot
-
     def _process_product_qty_gs1(self, product_qty):
         """Extend for custom processing of product qty."""
         return product_qty
 
-    def process_barcode_package(self, package_barcode, processed):
-        packaging = self.env["product.packaging"].search(
-            self._barcode_domain(package_barcode)
-        )
-        if not packaging:
-            self._set_messagge_info(
-                "not_found", _("Barcode for product packaging not found")
-            )
-            return False
+    def _process_ai_00(self, gs1_list):
+        """Packaging"""
+        return self.process_barcode_packaging_id()
+
+    def _process_ai_01(self, gs1_list):
+        """Packaging"""
+        return self.process_barcode_packaging_id()
+
+    def _process_ai_02(self, gs1_list):
+        """Product identification"""
+        res = self.process_barcode_product_id()
+        # If we did not found a product and we have not a package, maybe we
+        # can try to use this product barcode as a packaging barcode
+        if not res:
+            # Try to get packaging 01 with product GTIN
+            packaging_ai = next(filter(lambda f: f["ai"] == "01", gs1_list), False)
+            if not packaging_ai:
+                res = self._process_ai_01(gs1_list)
+        if not res:
+            # Try to get packaging 00 with product GTIN
+            packaging_ai = next(filter(lambda f: f["ai"] == "00", gs1_list), False)
+            if not packaging_ai:
+                res = self._process_ai_00(gs1_list)
+        return res
+
+    def _process_ai_240(self, gs1_list):
+        """Product identification"""
+        return self.process_barcode_product_id()
+
+    def _process_ai_10(self, gs1_list):
+        """Serial/Lot identification"""
+        self.lot_name = self.barcode
+        return self.process_barcode_lot_id()
+
+    def _process_ai_30(self, gs1_list):
+        """Variable Qty"""
+        product_qty = self._process_product_qty_gs1(float(self.barcode))
+        if self.packaging_id:
+            self.packaging_qty = product_qty
+            self.product_qty = self.packaging_qty * self.packaging_id.qty
         else:
-            if len(packaging) > 1:
-                self._set_messagge_info("more_match", _("More than one package found"))
-                return False
-            self.action_packaging_scaned_post(packaging)
+            self.product_qty = product_qty
+        return True
+
+    def _process_ai_37(self, gs1_list):
+        """Product Qty"""
+        product_qty = self._process_product_qty_gs1(float(self.barcode))
+        # if self.packaging_id:
+        #     product_qty = self.packaging_id.qty * product_qty
+        self.product_qty = product_qty
+        return True
+
+    def _process_ai_310(self, gs1_list):
+        """Net Weight"""
+        self.product_qty = self._process_product_qty_gs1(float(self.barcode))
+        return True
+
+    def _process_ai_330(self, gs1_list):
+        """Gross Weight"""
+        self.product_qty = self._process_product_qty_gs1(float(self.barcode))
+        return True
 
     def process_barcode(self, barcode):
-        """Only has been implemented AI (01, 02, 10, 37), so is possible that
-        scanner reads a barcode ok but this one is not precessed.
-        """
-        try:
-            barcode_decoded = self.env["gs1_barcode"].decode(barcode)
-        except Exception:
+        gs1_list = self.env.ref(
+            "barcodes_gs1_nomenclature.default_gs1_nomenclature"
+        ).parse_barcode(barcode)
+        if gs1_list is None:
             return super().process_barcode(barcode)
-        processed = False
-        package_barcode = barcode_decoded.get("01", False)
-        product_barcode = barcode_decoded.get("02", False)
-        if not product_barcode:
-            # Sometimes the product does not yet have a GTIN. In this case
-            # try the AI 240 'Additional product identification assigned
-            # by the manufacturer'.
-            product_barcode = barcode_decoded.get("240", False)
-        lot_barcode = barcode_decoded.get("10", False)
-        product_qty = barcode_decoded.get("37", False)
-        if product_barcode:
-            product = self.env["product.product"].search(
-                self._barcode_domain(product_barcode)
-            )
-            if not product and not package_barcode:
-                # If we did not found a product and we have not a package, maybe we
-                # can try to use this product barcode as a packaging barcode
-                package_barcode = product_barcode
-            elif not product:
-                self._set_messagge_info("not_found", _("Barcode for product not found"))
-                return False
+        warning_msg_list = []
+        self.message = False
+        for gs1_item in gs1_list:
+            self.barcode = gs1_item["value"]
+            ai = gs1_item["ai"]
+            if hasattr(self, "_process_ai_%s" % ai):
+                res = getattr(self, "_process_ai_%s" % ai[:3])(gs1_list=gs1_list)
+                if not res:
+                    warning_msg_list.append(
+                        self.message
+                        or _("({ai}){barcode} Not found").format(
+                            ai=ai, barcode=self.barcode
+                        )
+                    )
+                    self.message = False
             else:
-                processed = True
-                self.action_product_scaned_post(product)
-        if package_barcode:
-            value_returned = self.process_barcode_package(package_barcode, processed)
-            if value_returned is not None:
-                return value_returned
-        if lot_barcode and self.product_id.tracking != "none":
-            self.process_lot(barcode_decoded)
-            processed = True
-        if product_qty and package_barcode:
-            # If we have processed a package, we need to multiply it
-            product_qty = self._process_product_qty_gs1(product_qty)
-            self.product_qty = self.product_qty * product_qty
-        elif product_qty:
-            product_qty = self._process_product_qty_gs1(product_qty)
-            self.product_qty = product_qty
-        if not self.product_qty:
-            # This could happen with double GS1-128 barcodes
-            if self.packaging_id:
-                self.packaging_qty = 0.0 if self.manual_entry else 1.0
-                self.product_qty = self.packaging_id.qty * self.packaging_qty
-            else:
-                self.product_qty = 0.0 if self.manual_entry else 1.0
-        if processed:
-            if not self.check_option_required():
-                return False
-            self.action_confirm()
-            self._set_messagge_info("success", _("Barcode read correctly"))
-            return True
-        return super().process_barcode(barcode)
+                warning_msg_list.append(
+                    _("AI GS1 ({ai}) Not implemented").format(ai=ai)
+                )
+        if warning_msg_list:
+            self.barcode = False
+            self._set_messagge_info("info", " ".join(warning_msg_list))
+            return False
+        if not self.check_option_required():
+            return False
+        if self.is_manual_confirm or self.manual_entry:
+            self._set_messagge_info("info", _("Review and confirm"))
+            return False
+        return self.action_confirm()
