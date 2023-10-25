c4581d9f46f68ce8cb290b37a53043bee4600d64
sergio-teruel <sergio.teruel@tecnativa.com>
2023-10-25 18:00:47 +0200

    [IMP] stock_barcodes: Test improvements and create lots in confirm action

--- stock_barcodes/tests/test_stock_barcodes_new_lot.py
+++ stock_barcodes/tests/test_stock_barcodes_new_lot.py
@@ -6,11 +6,12 @@ from .test_stock_barcodes_inventory import TestStockBarcodesInventory
 
 
 @tagged("post_install", "-at_install")
-class TestStockBarcodesNewLot(TestStockBarcodesInventory):
-    def setUp(self):
-        super().setUp()
-        self.ScanReadLot = self.env["wiz.stock.barcodes.new.lot"]
-        self.wiz_scan_lot = self.ScanReadLot.new()
+class TestStockBarcodesNewLot(TestStockBarcodesPicking):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.ScanReadLot = cls.env["wiz.stock.barcodes.new.lot"]
+        cls.wiz_scan_lot = cls.ScanReadLot.new()
 
     def test_new_lot(self):
         self.action_barcode_scanned(self.wiz_scan_lot, "8433281006850")
