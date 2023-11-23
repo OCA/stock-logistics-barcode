f5880911c76ac6654c758f29951f1de7f4724a8c
sergio-teruel <sergio.teruel@tecnativa.com>
2023-11-23 17:36:22 +0100

    [IMP] stock_barcodes_gs1_secondary_unit: Use product secondary mixin

--- stock_barcodes_gs1_secondary_unit/wizard/stock_barcodes_read_todo.py
+++ stock_barcodes_gs1_secondary_unit/wizard/stock_barcodes_read_todo.py
@@ -1,42 +1,26 @@
 # Copyright 2023 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class WizStockBarcodesReadTodo(models.TransientModel):
-    _inherit = "wiz.stock.barcodes.read.todo"
-
-    secondary_uom_qty = fields.Float(
-        string="Secondary Qty",
-        digits="Product Unit of Measure",
-        readonly=True,
-    )
-    secondary_uom_id = fields.Many2one(
-        comodel_name="product.secondary.unit",
-        string="Second unit",
-        readonly=True,
-    )
+    _inherit = ["wiz.stock.barcodes.read.todo", "product.secondary.unit.mixin"]
+    _name = "wiz.stock.barcodes.read.todo"
+    _secondary_unit_fields = {
+        "qty_field": "product_uom_qty",
+        "uom_field": "uom_id",
+    }
 
     def _prepare_fill_record_values(self, wiz_barcode, line, position):
         vals = super()._prepare_fill_record_values(wiz_barcode, line, position)
-        vals.update(
-            {
-                "secondary_uom_qty": line.secondary_uom_qty,
-                "secondary_uom_id": line.secondary_uom_id.id,
-            }
-        )
-        return vals
-
-    def _update_fill_record_values(self, wiz_barcode, line, vals):
-        vals = super()._update_fill_record_values(wiz_barcode, line, vals)
-        vals["secondary_uom_qty"] += line.secondary_uom_qty
+        vals.update({"secondary_uom_id": line.secondary_uom_id.id})
         return vals
 
     @api.model
     def fields_to_fill_from_pending_line(self):
         res = super().fields_to_fill_from_pending_line()
-        res.extend(["secondary_uom_qty", "secondary_uom_id"])
+        res.append("secondary_uom_id")
         return res
 
     def _group_key(self, wiz, line):
