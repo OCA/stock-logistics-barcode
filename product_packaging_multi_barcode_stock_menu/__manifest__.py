a88942e7a9f8877bf5494ff9846742ac9f8e9cbd
Telmo Santos <telmo.santos@camptocamp.com>
2024-01-05 07:49:44 +0100

    [ADD] product_multi_barcode_stock_menu-view-packaging

---
+++ product_packaging_multi_barcode_stock_menu/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Multiple barcodes menu show product packaging",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptomcamp, Odoo Community Association (OCA)",
+    "category": "Product Management",
+    "depends": [
+        "product_packaging_multi_barcode",
+        "product_multi_barcode_stock_menu",
+    ],
+    "website": "https://github.com/OCA/stock-logistics-barcode",
+    "data": [
+        "views/barcode_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
