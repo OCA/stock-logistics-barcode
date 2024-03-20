cdb3dd86e9118a84071d24f596135a770e5d1e97
sergio-teruel <sergio.teruel@tecnativa.com>
2024-03-20 22:52:46 +0100

    [IMP] stock_picking_product_barcode_report: Add product packaging to wizard. New base report label to be extended by other modules. Label qty computed

--- stock_picking_product_barcode_report/__manifest__.py
+++ stock_picking_product_barcode_report/__manifest__.py
@@ -16,6 +16,7 @@
         "views/res_config_settings_view.xml",
         "data/paperformat_label.xml",
         "wizard/stock_barcode_selection_printing_view.xml",
+        "report/report_base_label.xml",
         "report/report_label_barcode.xml",
         "report/report_label_barcode_quant_package_template.xml",
         "report/report_label_barcode_template.xml",
