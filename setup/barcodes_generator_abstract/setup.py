eecb0928040fc5e76952802698a95a2086f81945
Isaac López <isaac@vauxoo.com>
2024-05-27 18:54:50 +0000

    [IMP] barcodes_generator_abstract: set 'python-barcode' as external_dependencies
    Since using the PyPI package has been the recommended way since v12.0
    (see https://github.com/odoo/odoo/pull/25549), this commit sets
    'python-barcode' as an external dependency and removes the
    'odoo_addon' key from setup.py

--- setup/barcodes_generator_abstract/setup.py
+++ setup/barcodes_generator_abstract/setup.py
@@ -2,12 +2,5 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon={
-        "external_dependencies_override": {
-            "python": {
-                "barcode": "python-barcode"
-            }
-        }
-    },
+    odoo_addon=True,
 )
-
