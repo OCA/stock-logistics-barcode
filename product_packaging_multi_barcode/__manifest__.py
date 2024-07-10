ed1a2175a50af486978ee9bfb2e15a4798ff8e2f
duongtq <duongtq@trobz.com>
2024-07-10 14:12:07 +0700

    [IMP] product_packaging_multi_barcode: remove packaging_id of product.barcode on product form view

--- product_packaging_multi_barcode/__manifest__.py
+++ product_packaging_multi_barcode/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Multiple barcodes on product packagings",
-    "version": "16.0.1.2.0",
+    "version": "16.0.1.3.0",
     "license": "AGPL-3",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "category": "Product Management",
@@ -10,7 +10,6 @@
     "website": "https://github.com/OCA/stock-logistics-barcode",
     "data": [
         "views/product_packaging_view.xml",
-        "views/product_template_view.xml",
         "views/barcode_view.xml",
     ],
     "installable": True,
