d6adeda4fc970333629114a61f722b9d6387e86e
Carlos Dauden <carlos.dauden@tecnativa.com>
2024-07-25 15:51:32 +0200

    [IMP] stock_barcodes_gs1_secondary_unit: Set todo secondary qty from move when all qty is available and only one move line TT50148

--- stock_barcodes_gs1_secondary_unit/wizard/stock_barcodes_read_todo.py
+++ stock_barcodes_gs1_secondary_unit/wizard/stock_barcodes_read_todo.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
+from odoo.tools import float_compare
 
 
 class WizStockBarcodesReadTodo(models.TransientModel):
@@ -14,7 +15,24 @@ class WizStockBarcodesReadTodo(models.TransientModel):
 
     def _prepare_fill_record_values(self, wiz_barcode, line, position):
         vals = super()._prepare_fill_record_values(wiz_barcode, line, position)
-        vals.update({"secondary_uom_id": line.secondary_uom_id.id})
+        vals["secondary_uom_id"] = line.secondary_uom_id.id
+        if line._name == "stock.move.line":
+            move = line.move_id
+            # Set secondary qty when stock.move full match with stock.move.line
+            if not (move.move_line_ids - line) and not float_compare(
+                move.product_uom_qty,
+                line.product_uom_qty,
+                precision_rounding=line.product_uom_id.rounding,
+            ):
+                vals["secondary_uom_qty"] = move.secondary_uom_qty
+        elif line._name == "stock.move":
+            # Set secondary qty when stock.move all quantity is available
+            if not float_compare(
+                line.reserved_availability,
+                line.product_uom_qty,
+                precision_rounding=line.product_uom.rounding,
+            ):
+                vals["secondary_uom_qty"] = line.secondary_uom_qty
         return vals
 
     @api.model
