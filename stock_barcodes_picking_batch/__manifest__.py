5763d80b40f00718c959e77ab278699a024939fb
sergio-teruel <sergio.teruel@tecnativa.com>
2024-07-29 12:26:37 +0200

    [MIG] stock_barcodes_picking_batch: Migration to v16.0

--- stock_barcodes_picking_batch/__manifest__.py
+++ stock_barcodes_picking_batch/__manifest__.py
@@ -1,9 +1,9 @@
 # Copyright 2021 Tecnativa - Sergio Teruel
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Barcodes Picking Batch",
     "summary": "It provides read barcodes on stock operations from batch pickings.",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-barcode",
     "license": "AGPL-3",
@@ -15,9 +15,11 @@
         "views/stock_picking_batch_views.xml",
         "wizard/stock_barcodes_read_picking_batch_views.xml",
     ],
-    "web.assets_backend": [
-        "stock_barcodes_picking_batch/static/src/**/*.js",
-    ],
+    "assets": {
+        "web.assets_backend": [
+            "stock_barcodes_picking_batch/static/src/**/*.js",
+        ],
+    },
     "installable": True,
     "auto_install": True,
 }
