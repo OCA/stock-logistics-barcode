d992e3b7e36de2f4e3b728352b73a8d04d7c9d41
sergio-teruel <sergio.teruel@tecnativa.com>
2024-07-29 12:22:28 +0200

    [MIG] stock_barcodes_picking_batch_revision: Migration to v16.0

--- stock_barcodes_picking_batch_revision/static/src/js/boolean_toggle_big.js
+++ stock_barcodes_picking_batch_revision/static/src/js/boolean_toggle_big.js
@@ -1,26 +1,12 @@
+/** @odoo-module **/
 /* Copyright 2024 Tecnativa - Carlos Roca
+/* Copyright 2024 Tecnativa - Sergio Teruel
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
-odoo.define(
-    "stock_barcodes_picking_batch_revision.boolean_toggle_big",
-    function (require) {
-        "use strict";
 
-        var basic_fields = require("web.basic_fields");
-        var field_registry = require("web.field_registry");
+import { registry } from "@web/core/registry";
+import {BooleanToggleField} from "@web/views/fields/boolean_toggle/boolean_toggle_field";
 
-        var FieldBarcodeBooleanToggleBig = basic_fields.BooleanToggle.extend({
-            /**
-             * Add class o_boolean_toggle_big to be able to modify css
-             *
-             * @override
-             * @private
-             */
-            _render: function () {
-                this._super.apply(this, arguments);
-                this.$el.addClass("o_boolean_toggle_big");
-            },
-        });
+class FieldBarcodeBooleanToggleBig extends BooleanToggleField {}
+FieldBarcodeBooleanToggleBig.template = "stock_barcodes_picking_batch_revision.FieldBarcodeBooleanToggleBig";
 
-        field_registry.add("boolean_toggle_big", FieldBarcodeBooleanToggleBig);
-    }
-);
+registry.category("fields").add("boolean_toggle_big", FieldBarcodeBooleanToggleBig);
