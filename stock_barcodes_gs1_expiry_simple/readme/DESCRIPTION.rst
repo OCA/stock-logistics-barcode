ded08e23fde519d210bd9cdd7c5f237c7a77c5e4
Alexis de Lattre <alexis.delattre@akretion.com>
2024-09-27 16:42:07 +0200

    [ADD] stock_barcodes_gs1_expiry_simple
    Add new glue module between stock_barcodes_gs1 and product_expiry_simple

---
+++ stock_barcodes_gs1_expiry_simple/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module is a glue module between the module **stock_barcodes** from `github.com/OCA/stock-logistics-barcode <https://github.com/OCA/stock-logistics-barcode/>`_ and the module **product_expiry_simple** from `github.com/OCA/stock-logistics-workflow <https://github.com/OCA/stock-logistics-workflow/>`_.
+
+The OCA module **product_expiry_simple** is an equivalent of the official module **product_expiry** that add a single date field on lots for the expiry date (instead of adding 4 datetime fields).
---
+++ stock_barcodes_gs1_expiry_simple/wizards/stock_barcodes_read.py
@@ -0,0 +1,26 @@
+# Copyright 2024 Akretion France (https://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class WizStockBarcodesRead(models.AbstractModel):
+    _inherit = "wiz.stock.barcodes.read"
+
+    expiry_date = fields.Date(readonly=True)
+    product_use_expiry_date = fields.Boolean(related="product_id.use_expiry_date")
+
+    def _process_ai_17(self, gs1_list):
+        self.expiry_date = self.barcode
+        return True
+
+    def _prepare_lot_vals(self):
+        vals = super()._prepare_lot_vals()
+        if self.expiry_date:
+            vals["expiry_date"] = self.expiry_date
+        return vals
+
+    def action_clean_lot(self):
+        self.expiry_date = False
+        return super().action_clean_lot()
