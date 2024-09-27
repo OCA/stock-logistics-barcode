b76fee601fce0d76c14d868f2e95c4dd7629508f
Carlos Dauden <carlos.dauden@tecnativa.com>
2024-09-27 18:10:05 +0200

    [IMP] stock_barcodes: Add pending move with difference between demand and reserved

--- stock_barcodes/models/__init__.py
+++ stock_barcodes/models/__init__.py
@@ -1,5 +1,6 @@
 from . import stock_barcodes_action
 from . import stock_barcodes_option
+from . import stock_move
 from . import stock_move_line
 from . import stock_picking
 from . import stock_picking_type
