9ead20cb5066abd76f9b06f5ab036700e63c9318
Carlos Dauden <carlos.dauden@tecnativa.com>
2024-09-30 23:49:49 +0200

    [IMP] stock_barcodes: Performance and extra checks from 16.0

--- stock_barcodes/hooks.py
+++ stock_barcodes/hooks.py
@@ -1,18 +1,20 @@
 # Copyright 2021 Tecnativa - Sergio Teruel
+# Copyright 2024 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import tools
 
 
 def pre_init_hook(cr):
-    if not tools.column_exists(cr, "stock_move_line", "barcode_scan_state"):
-        cr.execute(
-            """
-            ALTER TABLE stock_move_line
-            ADD COLUMN barcode_scan_state varchar"""
-        )
-        cr.execute(
-            """
-            UPDATE stock_move_line sml
-            SET barcode_scan_state = 'pending'
-            """
-        )
+    cr.execute(
+        """
+        ALTER TABLE stock_move_line
+            ADD COLUMN IF NOT EXISTS barcode_scan_state VARCHAR DEFAULT 'pending';
+        ALTER TABLE stock_move_line ALTER COLUMN barcode_scan_state DROP DEFAULT;
+        """,
+    )
+    cr.execute(
+        """
+        ALTER TABLE stock_move
+            ADD COLUMN IF NOT EXISTS barcode_backorder_action VARCHAR DEFAULT 'pending';
+        ALTER TABLE stock_move ALTER COLUMN barcode_backorder_action DROP DEFAULT;
+        """,
+    )
