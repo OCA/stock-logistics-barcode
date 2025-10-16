bbf450595c2737ad9765a332a433e615d08954ea
CarlosRoca13 <carlos.roca@tecnativa.com>
2025-10-16 17:13:53 +0200

    [IMP] stock_barcodes: Use qty_picked field instead of module stock_move_line_qty_picked

---
+++ stock_barcodes/migrations/18.0.1.0.0/pre-migration.py
@@ -0,0 +1,13 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        ALTER TABLE stock_move_line
+            ADD COLUMN IF NOT EXISTS qty_picked numeric;
+        UPDATE stock_move_line set qty_picked = qty_done;
+        """,
+    )
