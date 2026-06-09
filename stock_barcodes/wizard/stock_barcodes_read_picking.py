55accb6a16fdd4e8c27ddadc24686505f1dba1ed
sergio-teruel <sergio.teruel@tecnativa.com>
2026-06-09 17:58:12 +0200

    [IMP] stock_barcodes: always take into account qty_picked to evaluate max_quantity TT63021

--- stock_barcodes/wizard/stock_barcodes_read_picking.py
+++ stock_barcodes/wizard/stock_barcodes_read_picking.py
@@ -550,11 +550,7 @@ class WizStockBarcodesReadPicking(models.TransientModel):
             if candidate_domain:
                 lines = lines.filtered_domain(candidate_domain)
         available_qty = self.product_qty
-        max_quantity = sum(
-            sm.product_uom_qty
-            - (sm.qty_picked if sm.qty_picked != sm.product_uom_qty else 0)
-            for sm in moves_todo
-        )
+        max_quantity = sum(sm.product_uom_qty - sm.qty_picked for sm in moves_todo)
         if (
             not self.option_group_id.code == "REL"
             and not self.env.context.get("force_create_move", False)
