b9858759fef95d8619ba7b423f02e5c604a098fa
Carlos Dauden <carlos.dauden@tecnativa.com>
2026-07-08 18:38:09 +0200

    [REF] stock_barcodes: Remove dead barcode_reload listener
    The barcode handler field subscribed to the 'barcode_reload' bus channel
    that no code ever sends to, and listened through
    busService.addEventListener('notification'), an event the 18.0 bus
    service no longer triggers for notifications. The whole block (and the
    unused orm service) was dead code.

--- stock_barcodes/static/src/utils/barcode_handler_field.esm.js
+++ stock_barcodes/static/src/utils/barcode_handler_field.esm.js
@@ -1,62 +1,11 @@
 /* @odoo-module */
 
-import {useEffect} from "@odoo/owl";
-import {useService} from "@web/core/utils/hooks";
-import {patch} from "@web/core/utils/patch";
 import {BarcodeHandlerField} from "@barcodes/barcode_handler_field";
+import {patch} from "@web/core/utils/patch";
 
-const _origSetup = BarcodeHandlerField.prototype.setup;
 const _origOnBarcodeScanned = BarcodeHandlerField.prototype.onBarcodeScanned;
 
 patch(BarcodeHandlerField.prototype, {
-    setup() {
-        if (typeof _origSetup === "function") {
-            _origSetup.call(this, ...arguments);
-        }
-
-        // Services en 18.0
-        this.busService = useService("bus_service");
-        this.orm = useService("orm");
-
-        // Handler estable (misma referencia para subscribe/unsubscribe)
-        this._onBusNotification = async (notifications = []) => {
-            for (const {type} of notifications) {
-                if (type === "stock_barcodes_refresh_data") {
-                    const model = this.env?.model || this.props?.record?.model;
-                    const rootModel = model?.root || this.env?.model?.root;
-                    if (rootModel?.load) {
-                        await rootModel.load();
-                    }
-                    if (model?.notify) {
-                        model.notify();
-                    }
-                }
-            }
-        };
-
-        // Importante en OWL v2: deps como función que devuelve []
-        useEffect(
-            () => {
-                // Suscribir al canal y al evento correcto
-                this.busService.addChannel("barcode_reload");
-                this.busService.addEventListener(
-                    "notification",
-                    this._onBusNotification
-                );
-
-                // Limpieza
-                return () => {
-                    this.busService.deleteChannel("barcode_reload");
-                    this.busService.removeEventListener(
-                        "notification",
-                        this._onBusNotification
-                    );
-                };
-            },
-            () => []
-        );
-    },
-
     onBarcodeScanned(event) {
         if (typeof _origOnBarcodeScanned === "function") {
             _origOnBarcodeScanned.call(this, event);
